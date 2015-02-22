c9ba9a6777b9eac58f8b9a7fb6d5dedb5da2cdbf
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-02-22 14:21:14 +0000

    magic string fix

--- trunk/sk1-wx/src/uc2/formats/sk1/__init__.py
+++ trunk/sk1-wx/src/uc2/formats/sk1/__init__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2013 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -51,8 +51,8 @@ def check_sk1(path):
 		events.emit(events.MESSAGES, msgconst.ERROR, msg)
 		raise IOError(errtype, msg + '\n' + value, traceback)
 
-	string = file.read(5)
+	string = file.read(7)
 
 	file.close()
-	if string == '##sK1': return True
+	if string == '##sK1 1': return True
 	return False
