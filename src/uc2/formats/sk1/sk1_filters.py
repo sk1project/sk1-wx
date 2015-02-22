38c58654a29e1622627f3e988e8d9f8d812e3354
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-02-22 17:47:04 +0000

    loading progress improvement

--- src/uc2/formats/sk1/sk1_filters.py
+++ src/uc2/formats/sk1/sk1_filters.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2013 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -54,11 +54,9 @@ class SK1_Loader(AbstractLoader):
 			self.line = self.file.readline()
 			if not self.line: break
 			self.line = self.line.rstrip('\r\n')
-			position = float(self.file.tell()) / float(self.file_size) * 0.95
-			if position - self.position > 0.01:
-				self.position = position
-				msg = _('Parsing in process...')
-				events.emit(events.FILTER_INFO, msg, position)
+
+			self.check_loading()
+
 			if self.line:
 				try:
 					code = compile('self.' + self.line, '<string>', 'exec')
