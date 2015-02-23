da96d5badb156514edc3389129c9fc22923b41ba
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-02-23 12:46:56 +0000

    PLT translator to sk2 model

--- trunk/sk1-wx/src/uc2/formats/plt/presenter.py
+++ trunk/sk1-wx/src/uc2/formats/plt/presenter.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,8 +22,8 @@ from uc2.formats.generic import TextModelPresenter
 from uc2.formats.plt.plt_config import PLT_Config
 from uc2.formats.plt import model
 from uc2.formats.plt.plt_filters import PLT_Loader, PLT_Saver
-from uc2.formats.plt.plt_translators import PDXF_to_PLT_Translator
-from uc2.formats.plt.plt_translators import PLT_to_PDXF_Translator
+from uc2.formats.plt.plt_translators import SK2_to_PLT_Translator
+from uc2.formats.plt.plt_translators import PLT_to_SK2_Translator
 
 class PLT_Presenter(TextModelPresenter):
 
@@ -54,13 +54,13 @@ class PLT_Presenter(TextModelPresenter):
 	def get_jobs(self):
 		return self.model.childs[1].childs
 
-	def traslate_from_pdxf(self, pdxf_doc):
-		translator = PDXF_to_PLT_Translator()
-		model = pdxf_doc.model
+	def traslate_from_pdxf(self, sk2_doc):
+		translator = SK2_to_PLT_Translator()
+		model = sk2_doc.model
 		objs = [] + model.childs[0].childs[0].childs + model.childs[1].childs
 		translator.translate(objs, self)
 
-	def traslate_to_pdxf(self, pdxf_doc):
-		translator = PLT_to_PDXF_Translator()
-		translator.translate(self, pdxf_doc)
+	def traslate_to_sk2(self, sk2_doc):
+		translator = PLT_to_SK2_Translator()
+		translator.translate(self, sk2_doc)
 
