189e8d3f4c6e13d30e260d05ebe81243b8dbf461
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-02-23 10:51:08 +0000

    CDR translator to sk2 model

--- trunk/sk1-wx/src/uc2/formats/cdr/__init__.py
+++ trunk/sk1-wx/src/uc2/formats/cdr/__init__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2011-2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -20,18 +20,18 @@ import sys
 from uc2 import events, msgconst
 from uc2.formats.cdr.presenter import CDR_Presenter
 from uc2.formats.cdr import const
-from uc2.formats.pdxf.presenter import PDXF_Presenter
+from uc2.formats.sk2.sk2_presenter import SK2_Presenter
 
 def cdr_loader(appdata, filename, translate=True, cnf={}, **kw):
 	if kw: cnf.update(kw)
 	doc = CDR_Presenter(appdata, cnf)
 	doc.load(filename)
 	if translate:
-		pdxf_doc = PDXF_Presenter(appdata, cnf)
-		pdxf_doc.doc_file = filename
-		doc.traslate_to_pdxf(pdxf_doc)
+		sk2_doc = SK2_Presenter(appdata, cnf)
+		sk2_doc.doc_file = filename
+		doc.traslate_to_sk2(sk2_doc)
 		doc.close()
-		doc = pdxf_doc
+		doc = sk2_doc
 	return doc
 
 def cdr_saver(cdr_doc, filename, translate=True, cnf={}, **kw):
@@ -40,15 +40,15 @@ def cdr_saver(cdr_doc, filename, translate=True, cnf={}, **kw):
 
 def check_cdr(path):
 	try:
-		file = open(path, 'rb')
+		fileptr = open(path, 'rb')
 	except:
 		errtype, value, traceback = sys.exc_info()
 		msg = _('Cannot open %s file for reading') % (path)
 		events.emit(events.MESSAGES, msgconst.ERROR, msg)
 		raise IOError(errtype, msg + '\n' + value, traceback)
 
-	header = file.read(12)
-	file.close()
+	header = fileptr.read(12)
+	fileptr.close()
 	if not header[:4] == 'RIFF':
 		return False
 	if header[8:] in const.CDR_VERSIONS:
--- trunk/sk1-wx/src/uc2/formats/cdr/presenter.py
+++ trunk/sk1-wx/src/uc2/formats/cdr/presenter.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,7 +22,7 @@ from uc2.formats.generic import BinaryModelPresenter
 from uc2.formats.cdr.cdr_config import CDR_Config
 from uc2.formats.riff import model
 from uc2.formats.cdr.cdr_filters import CDR_Loader, CDR_Saver
-from uc2.formats.cdr.cdr_translators import CDR_to_PDXF_Translator
+from uc2.formats.cdr.cdr_translators import CDR_to_SK2_Translator
 
 class CDR_Presenter(BinaryModelPresenter):
 
@@ -50,11 +50,11 @@ class CDR_Presenter(BinaryModelPresenter):
 	def load(self, path):
 		BinaryModelPresenter.load(self, path)
 
-	def traslate_from_pdxf(self, pdxf_doc):
+	def traslate_from_sk2(self, sk2_doc):
 		pass
 
-	def traslate_to_pdxf(self, pdxf_doc):
+	def traslate_to_sk2(self, sk2_doc):
 		msg = _('Translation is under process...')
 		events.emit(events.FILTER_INFO, msg, 0.95)
-		translator = CDR_to_PDXF_Translator()
-		translator.translate(self, pdxf_doc)
+		translator = CDR_to_SK2_Translator()
+		translator.translate(self, sk2_doc)
