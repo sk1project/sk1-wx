6340b2c307f8f0bb3dcec5c2d0b84875ebf7d635
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-03-02 17:43:56 +0000

    fallback loader connected

--- src/uc2/formats/__init__.py
+++ src/uc2/formats/__init__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -23,6 +23,7 @@ from uc2 import events, msgconst
 from uc2.utils.fs import get_file_extension
 
 from uc2.formats import data
+from uc2.formats.fallback import fallback_check, im_loader
 
 
 def get_loader(path, experimental=False):
@@ -39,24 +40,32 @@ def get_loader(path, experimental=False):
 
 	if experimental:
 		ld_formats += data.EXPERIMENTAL_LOADERS
-	for format in ld_formats:
-		if ext in uc2const.FORMAT_EXTENSION[format]:
-			if data.CHECKERS[format](path):
-				loader = data.LOADERS[format]
+	for item in ld_formats:
+		if ext in uc2const.FORMAT_EXTENSION[item]:
+			if data.CHECKERS[item](path):
+				loader = data.LOADERS[item]
 				break
-	if loader is None:
 
+	if loader is None:
 		msg = _('Loader is not found or not suitable for %s') % (path)
 		events.emit(events.MESSAGES, msgconst.WARNING, msg)
 		msg = _('Start to search loader by file content')
 		events.emit(events.MESSAGES, msgconst.INFO, msg)
 
-		for format in ld_formats:
-			checker = data.CHECKERS[format]
+		for item in ld_formats:
+			checker = data.CHECKERS[item]
 			if not checker is None:
 				if checker(path):
-					loader = data.LOADERS[format]
+					loader = data.LOADERS[item]
 					break
+
+	if loader is None:
+		msg = _('By file content loader is not found for %s') % (path)
+		events.emit(events.MESSAGES, msgconst.WARNING, msg)
+		msg = _('Try using fallback loader')
+		events.emit(events.MESSAGES, msgconst.INFO, msg)
+		if fallback_check(path): loader = im_loader
+
 	if loader is None:
 		msg = _('Loader is not found for %s') % (path)
 		events.emit(events.MESSAGES, msgconst.ERROR, msg)
@@ -75,9 +84,9 @@ def get_saver(path, experimental=False):
 
 	if experimental:
 		sv_formats += data.EXPERIMENTAL_SAVERS
-	for format in sv_formats:
-		if ext in uc2const.FORMAT_EXTENSION[format]:
-			saver = data.SAVERS[format]
+	for item in sv_formats:
+		if ext in uc2const.FORMAT_EXTENSION[item]:
+			saver = data.SAVERS[item]
 			break
 	if saver is None:
 		msg = _('Saver is not found for %s file format') % (ext.__str__())
@@ -93,4 +102,4 @@ def _test():
 
 
 if __name__ == '__main__':
-    _test()
+	_test()
