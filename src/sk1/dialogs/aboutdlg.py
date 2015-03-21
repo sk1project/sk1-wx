f0284fb6014d5a04d93336dd5c23a0ec0913b87b
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-03-21 18:43:00 +0000

    AboutDialog refactoring

--- src/sk1/dialogs/aboutdlg.py
+++ src/sk1/dialogs/aboutdlg.py
@@ -17,16 +17,16 @@
 
 import wx
 
-from wal import const, EXPAND, ALL, LEFT, CENTER
-from wal import HPanel, VPanel, Notebook, Label, HtmlLabel
-from wal import Entry
+import wal
+from wal import const, EXPAND
+from wal import VPanel
 
 from sk1 import _
-from sk1.resources import icons
+from sk1.resources import icons, get_bmp
 from sk1.dialogs.license import LICENSE
 from sk1.dialogs.credits import CREDITS
 
-class PDAboutDialog(wx.Dialog):
+class AboutDialog(wal.SimpleDialog):
 
 	sizer = None
 	app = None
@@ -34,67 +34,60 @@ class PDAboutDialog(wx.Dialog):
 	def __init__(self, app, parent, title, size=(500, 350)):
 		self.app = app
 		if const.is_mac(): size = (550, 400)
-		wx.Dialog.__init__(self, parent, -1, title, wx.DefaultPosition, size)
-		self.sizer = wx.BoxSizer(wx.VERTICAL)
-		self.SetSizer(self.sizer)
+		wal.SimpleDialog.__init__(self, parent, title, size)
 
-		margin = 5
-		if not const.is_gtk(): margin = 10
+	def build(self):
+		header = AboutHeader(self.app, self)
+		self.pack(header, fill=True, padding=5)
 
-		self.box = VPanel(self, space=margin)
-		self.sizer.Add(self.box, 1, ALL | EXPAND)
-
-		header = AboutHeader(self.app, self.box)
-		self.box.add(header, 0, ALL | EXPAND, 5)
-
-		nb = Notebook(self.box)
+		nb = wal.Notebook(self)
 		nb.add_page(AboutPage(self.app, nb), _('About'))
 		nb.add_page(ComponentsPage(self.app, nb), _('Components'))
 		nb.add_page(AuthorsPage(nb), _('Authors'))
 		nb.add_page(ThanksPage(nb), _('Thanks to'))
 		nb.add_page(LicensePage(nb), _('License'))
-		self.box.add(nb, 1, ALL | EXPAND, 5)
+		self.pack(nb, expand=True, fill=True, padding=5)
 
 
-class AboutHeader(HPanel):
+class AboutHeader(wal.VPanel):
 
 	def __init__(self, app, parent):
-		HPanel.__init__(self, parent)
+		wal.VPanel.__init__(self, parent)
 		self.set_bg(const.UI_COLORS['pressed_border'])
 
-		panel = HPanel(self)
+		panel = wal.HPanel(self)
 		color = const.lighter_color(const.UI_COLORS['bg'], 0.9)
 		panel.set_bg(color)
-		bmp = wx.ArtProvider.GetBitmap(icons.PDESIGN_ICON32, size=const.DEF_SIZE)
-		bitmap = wx.StaticBitmap(panel, -1, bmp)
-		panel.add(bitmap, 0, ALL, 5)
+		panel.pack(get_bmp(panel, icons.PDESIGN_ICON32), padding=5)
 
 		data = app.appdata
 
-		p = VPanel(panel)
+		p = wal.VPanel(panel)
 		p.set_bg(color)
-		p.add(Label(p, data.app_name, True, 3), 0, ALL | EXPAND, 0)
+		p.pack(wal.Label(p, data.app_name, True, 3), fill=True)
 		txt = ('%s: %s %s') % (_('Version'), data.version, data.revision)
-		p.add(Label(p, txt), 0, ALL | EXPAND, 0)
-		panel.add(p, 0, LEFT | CENTER, 0)
+		p.pack(wal.Label(p, txt), fill=True)
+		panel.pack(p)
 
-		self.add(panel, 1, ALL | EXPAND, 1)
+		self.add(panel, 1, wal.ALL | wal.EXPAND , 1)
 
-class AboutPage(HPanel):
+class AboutPage(wal.HPanel):
 
 	def __init__(self, app, parent):
-		HPanel.__init__(self, parent)
-		self.add((50, 10))
-		box = VPanel(self)
-		self.add(box, 0, LEFT | CENTER, 5)
+		wal.HPanel.__init__(self, parent)
+		self.pack((50, 10))
+		box = wal.VPanel(self)
+		self.pack(box, padding=5)
 		data = app.appdata
 		txt = data.app_name + ' - ' + _('vector graphics editor') + '\n'
-		box.add(Label(box, txt, True, 2))
+		box.pack(wal.Label(box, txt, True, 2), fill=True)
 		import datetime
 		year = str(datetime.date.today().year)
 		txt = '(C) 2011-' + year + ' sK1 Project team' + '\n'
-		box.add(Label(box, txt))
-		box.add(HtmlLabel(box, 'http://sk1project.org'))
+		box.pack(wal.Label(box, txt), fill=True)
+		p = wal.HPanel(box)
+		p.pack(wal.HtmlLabel(p, 'http://sk1project.org'))
+		box.pack(p, fill=True)
 
 class ComponentsPage(VPanel):
 
@@ -120,10 +113,10 @@ class ComponentsPage(VPanel):
 		lc.SetColumnWidth(0, wx.LIST_AUTOSIZE)
 		lc.SetColumnWidth(1, 500)
 
-class AuthorsPage(VPanel):
+class AuthorsPage(wal.VPanel):
 
 	def __init__(self, parent):
-		VPanel.__init__(self, parent)
+		wal.VPanel.__init__(self, parent)
 		sep = "------------------------------\n"
 		dev = "\nIgor E. Novikov\n"
 		dev += "(sK1 2.0, wxWidgets version; sK1, Tk version)\n"
@@ -132,26 +125,26 @@ class AuthorsPage(VPanel):
 		dev += '\n' + sep
 		dev += "Bernhard Herzog (Skencil, Tk version)\n"
 		dev += "<bernhard@users.sourceforge.net>\n" + sep
-		entry = Entry(self, dev, multiline=True, editable=False)
-		self.add(entry, 1, ALL | EXPAND, 5)
+		entry = wal.Entry(self, dev, multiline=True, editable=False)
+		self.pack(entry, expand=True, fill=True, padding=5)
 
-class ThanksPage(VPanel):
-
-	def __init__(self, parent):
-		VPanel.__init__(self, parent)
-		entry = Entry(self, CREDITS, multiline=True, editable=False)
-		self.add(entry, 1, ALL | EXPAND, 5)
-
-class LicensePage(VPanel):
+class ThanksPage(wal.VPanel):
 
 	def __init__(self, parent):
-		VPanel.__init__(self, parent)
-		entry = Entry(self, LICENSE, multiline=True, editable=False)
-		self.add(entry, 1, ALL | EXPAND, 5)
+		wal.VPanel.__init__(self, parent)
+		entry = wal.Entry(self, CREDITS, multiline=True, editable=False)
+		self.pack(entry, expand=True, fill=True, padding=5)
+
+class LicensePage(wal.VPanel):
+
+	def __init__(self, parent):
+		wal.VPanel.__init__(self, parent)
+		entry = wal.Entry(self, LICENSE, multiline=True, editable=False)
+		self.pack(entry, expand=True, fill=True, padding=5)
 
 def about_dialog(app, parent):
 	title = _('About') + ' ' + app.appdata.app_name
-	dlg = PDAboutDialog(app, parent, title)
+	dlg = AboutDialog(app, parent, title)
 	dlg.Centre()
 	dlg.ShowModal()
 	dlg.Destroy()
