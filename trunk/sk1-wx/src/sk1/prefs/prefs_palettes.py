d6011338d326695d31001b543618e9aff008991c
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-03-27 14:23:58 +0000

    palette name and orientation options

--- trunk/sk1-wx/src/sk1/prefs/prefs_palettes.py
+++ trunk/sk1-wx/src/sk1/prefs/prefs_palettes.py
@@ -17,10 +17,12 @@
 
 import wal
 
-from sk1 import _
+from sk1 import _, config
 from sk1.resources import icons
 
 from generic import PrefPanel
+
+PAL_ORIENT = [_('Horizontal'), _('Vertical')]
 
 class PalettesPrefs(PrefPanel):
 
@@ -33,10 +35,43 @@ class PalettesPrefs(PrefPanel):
 		PrefPanel.__init__(self, app, dlg)
 
 		self.nb = wal.Notebook(self)
+
+		#========Palette options
 		pal_opt = wal.VPanel(self.nb)
+		pal_opt.pack((10, 10))
+
+		grid = wal.GridPanel(self, hgap=5, vgap=5)
+		grid.add_growable_col(1)
+
+		txt = _('Current palette:')
+		grid.pack(wal.Label(grid, txt))
+		pal_list = self.app.palettes.palettes.keys()
+		self.pal = wal.Combolist(grid, items=pal_list, onchange=None)
+		curent_palette = self.app.palettes.palette_in_use.model.name
+		self.pal.set_active(pal_list.index(curent_palette))
+		grid.pack(self.pal, fill=True)
+
+		txt = _('Palette orientation:')
+		grid.pack(wal.Label(grid, txt))
+		self.pal_orient = wal.Combolist(grid, items=PAL_ORIENT)
+		self.pal_orient.set_active(config.palette_orientation)
+		grid.pack(self.pal_orient)
+
+		pal_opt.pack(grid, fill=True, padding_all=5)
+
+
 		self.nb.add_page(pal_opt, _('Palette options'))
 
-		pal_mngr = wal.VPanel(self.nb)
-		self.nb.add_page(pal_mngr, _('Palette management'))
+		#========Palette management
+		self.nb.add_page(wal.VPanel(self.nb), _('Palette management'))
 
 		self.pack(self.nb, expand=True, fill=True)
+		self.built = True
+
+	def apply_changes(self):
+		print self.pal_orient.get_active()
+		config.palette_orientation = self.pal_orient.get_active()
+
+	def restore_defaults(self):
+		defaults = config.get_defaults()
+		self.pal_orient.get_active(defaults['palette_orientation'])
