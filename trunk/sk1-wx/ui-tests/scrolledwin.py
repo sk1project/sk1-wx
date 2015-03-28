5b90501c49659229e7919828050e7d458e9c9f65
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-03-28 14:16:20 +0000

    SrolledWindow widget

---
+++ trunk/sk1-wx/ui-tests/scrolledwin.py
@@ -0,0 +1,25 @@
+import wal
+
+
+class WidgetPanel(wal.HPanel):
+	def __init__(self, parent):
+		wal.HPanel.__init__(self, parent)
+
+		self.win = wal.ScrolledPanel(self, border=True)
+#		self.win.set_virtual_size((160, 1600))
+		self.win.set_bg(wal.WHITE)
+
+		self.pack(self.win, expand=True, fill=True, padding_all=10)
+
+	def selected(self, item):
+		print item
+
+	def activated(self, item):
+		print item
+
+app = wal.Application('wxWidgets')
+mw = wal.MainWindow('List widget', (250, 350))
+panel = WidgetPanel(mw)
+mw.pack(panel, expand=True, fill=True, padding=10)
+app.mw = mw
+app.run()
