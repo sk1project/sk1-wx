330bf2429263a49acc0307fff961b182b9227cba
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-03-30 13:01:32 +0000

    minor fix

--- src/sword/dialogs/__init__.py
+++ src/sword/dialogs/__init__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -249,6 +249,6 @@ class ProgressDialog(gtk.Dialog):
 
 	def destroy(self):
 		events.disconnect(events.FILTER_INFO, self.listener)
-		gobject.source_remove(self.timer)
+#		gobject.source_remove(self.timer)
 		self.timer = 0
 		gtk.Dialog.destroy(self)
