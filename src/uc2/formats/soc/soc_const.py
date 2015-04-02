8b122b39da49cecd1d81f9ee92fbe5a60643fcef
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-04-02 15:26:01 +0000

    SOC palette implementation

---
+++ src/uc2/formats/soc/soc_const.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2012 by Igor E. Novikov
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+SOURCE_LO = 'LibreOffice, The Document Foundation (http://www.libreoffice.org/)'
+SOURCE_OO = 'Apache OpenOffice (http://www.openoffice.org/)'
+
+SOC_PAL_TAG = 'office:color-table'
+SOC_COLOR_TAG = 'draw:color'
+SOC_COLOR_NAME_ATTR = 'draw:name'
+SOC_COLOR_VAL_ATTR = 'draw:color'
+
+SOC_PAL_ATTRS = {
+"xmlns:office":"http://openoffice.org/2000/office",
+"xmlns:style":"http://openoffice.org/2000/style",
+"xmlns:text":"http://openoffice.org/2000/text",
+"xmlns:table":"http://openoffice.org/2000/table",
+"xmlns:draw":"http://openoffice.org/2000/drawing",
+"xmlns:fo":"http://www.w3.org/1999/XSL/Format",
+"xmlns:xlink":"http://www.w3.org/1999/xlink",
+"xmlns:dc":"http://purl.org/dc/elements/1.1/",
+"xmlns:meta":"http://openoffice.org/2000/meta",
+"xmlns:number":"http://openoffice.org/2000/datastyle",
+"xmlns:svg":"http://www.w3.org/2000/svg",
+"xmlns:chart":"http://openoffice.org/2000/chart",
+"xmlns:dr3d":"http://openoffice.org/2000/dr3d",
+"xmlns:math":"http://www.w3.org/1998/Math/MathML",
+"xmlns:form":"http://openoffice.org/2000/form",
+"xmlns:script":"http://openoffice.org/2000/script",
+"xmlns:config":"http://openoffice.org/2001/config",
+}
---
+++ src/uc2/formats/soc/soc_filters.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2015 by Igor E. Novikov
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from uc2.formats.generic_filters import AbstractXMLLoader, AbstractSaver
+from uc2.formats.soc.soc_const import SOC_COLOR_TAG, SOC_COLOR_NAME_ATTR, \
+SOC_COLOR_VAL_ATTR, SOC_PAL_TAG, SOC_PAL_ATTRS
+
+class SOC_Loader(AbstractXMLLoader):
+
+	name = 'SOC_Loader'
+
+	def do_load(self):
+		self.start_parsing()
+
+	def start_element(self, name, attrs):
+		if name == SOC_COLOR_TAG:
+			self.model.colors.append([attrs._attrs[SOC_COLOR_VAL_ATTR],
+				attrs._attrs[SOC_COLOR_NAME_ATTR]])
+
+class SOC_Saver(AbstractSaver):
+
+	name = 'SOC_Saver'
+
+	def do_save(self):
+		self.writeln('<?xml version="1.0" encoding="UTF-8"?>')
+		self.writeln('')
+		if self.model.comments:
+			self.writeln('<!--')
+			self.fileptr.write(self.model.comments)
+			self.writeln('-->')
+
+		line = '<%' % SOC_PAL_TAG
+		for item in SOC_PAL_ATTRS.keys():
+			line += ' %s="%s"' % (item, SOC_PAL_ATTRS[item])
+		self.writeln(line + '>')
+
+		for item in self.model.colors:
+			self.writeln(' <%s %s="%s" %s="%"/>' % (SOC_COLOR_TAG,
+				SOC_COLOR_NAME_ATTR, item[1], SOC_COLOR_VAL_ATTR, item[0]))
+
+		self.writeln('</%s>' % SOC_PAL_TAG)
