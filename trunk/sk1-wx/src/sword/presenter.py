9651efd1f423c952f10d4a30bfd3c174ca464614
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-04-02 10:45:41 +0000

    fix to support palette hub

--- trunk/sk1-wx/src/sword/presenter.py
+++ trunk/sk1-wx/src/sword/presenter.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 #
 #	Copyright (C) 2012 by Igor E. Novikov
-#	
+#
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-#	
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
-#	
+#
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -50,7 +50,7 @@ class SW_Presenter:
 				raise IOError(_('Unknown file format'), doc_file)
 
 			pd = ProgressDialog(_('Opening file...'), self.app.mw)
-			ret = pd.run(loader, [app.appdata, doc_file, False])
+			ret = pd.run(loader, [app.appdata, doc_file, None, False, False])
 			if ret == gtk.RESPONSE_OK:
 				if pd.result is None:
 					pd.destroy()
