3c3e86803dabc0b57917698ec4c974365df14af7
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-04-04 11:48:52 +0000

    CPL format

---
+++ src/uc2/formats/cpl/cpl_filters.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+#	Copyright (C) 2015 by Igor E. Novikov
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from uc2 import cms
+from uc2.formats.generic_filters import AbstractLoader, AbstractSaver
+
+class CPL_Loader(AbstractLoader):
+
+	name = 'CPL_Loader'
+
+	def do_load(self):pass
+
+class CPL_Saver(AbstractSaver):
+
+	name = 'CPL_Saver'
+
+	def do_save(self):pass
