da463e35abff8d402eab4175a9d75a208d44a1d9
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-04-04 09:25:26 +0000

    refactoring

--- trunk/sk1-wx/src/uc2/formats/skp/skp_const.py
+++ trunk/sk1-wx/src/uc2/formats/skp/skp_const.py
@@ -15,4 +15,4 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-SKP_HEADER = '##sK1 palette'
+SKP_ID = '##sK1 palette'
