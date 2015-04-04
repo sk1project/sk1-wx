196693b068bcbe93f1f44ed133a3c6a5b1fa274c
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-04-04 16:14:28 +0000

    CPL10 model and loader

--- src/uc2/formats/cpl/cpl_filters.py
+++ src/uc2/formats/cpl/cpl_filters.py
@@ -18,13 +18,40 @@
 import os
 
 from uc2 import cms
-from uc2.formats.generic_filters import AbstractLoader, AbstractSaver
+from uc2.formats.generic_filters import AbstractBinaryLoader, AbstractSaver
+from uc2.formats.cpl import cpl_const
+from uc2.formats.cpl.cpl_model import CPL10_Palette, CPL10_Color
 
-class CPL_Loader(AbstractLoader):
+class CPL_Loader(AbstractBinaryLoader):
 
 	name = 'CPL_Loader'
 
-	def do_load(self):pass
+	def do_load(self):
+		ver = self.readbytes(2)
+		if ver == cpl_const.CPL10: self.do_cpl10_load()
+		if not self.model.name and self.doc_file:
+			name = os.path.basename(self.filepath).replace('.cpl', '')
+			self.model.name = name + ' palette'
+
+	def do_cpl10_load(self):
+		self.model = CPL10_Palette()
+		size = self.readbyte()
+		self.model.name = self.readstr(size)
+		ncolors = self.readword()
+		self.fileptr.seek(0)
+		self.model.chunk = self.readbytes(2 + 1 + size + 2)
+		for i in range(ncolors):
+			color = CPL10_Color()
+			color.model = self.readword()
+			color.valbytes = self.readbytes(10)
+			size = self.readbyte()
+			color.name = self.readstr(size)
+			ln = 2 + 10 + 1 + size
+			self.fileptr.seek(-ln, 1)
+			color.chunk = self.readbytes(ln)
+			self.model.childs.append(color)
+
+
 
 class CPL_Saver(AbstractSaver):
 
