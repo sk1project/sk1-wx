ec22307373764f16bb9da784dbb2a01f9fa4543c
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-04-06 17:02:25 +0000

    viewer refactoring to Canvas interfaces

--- trunk/sk1-wx/src/sk1/pwidgets/palette_viewer.py
+++ trunk/sk1-wx/src/sk1/pwidgets/palette_viewer.py
@@ -15,9 +15,11 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import wal, wx
+import wal
 
-class ScrolledPalette(wal.ScrolledPanel):
+from sk1 import _
+
+class ScrolledPalette(wal.ScrolledPanel, wal.Canvas):
 
 	width = 10
 	height = 10
@@ -30,16 +32,17 @@ class ScrolledPalette(wal.ScrolledPanel):
 	def __init__(self, app, parent):
 		self.app = app
 		self.parent = parent
-		wal.ScrolledPanel.__init__(self, parent, drawable=True)
+		wal.ScrolledPanel.__init__(self, parent)
+		wal.Canvas.__init__(self)
 		sb = wal.ScrollBar(self)
 		self.sb_width = sb.get_size()[0]
-		sb.Destroy()
+		sb.destroy()
 		self.width = (self.cell_width - 1) * self.cell_in_line + 3 + self.sb_width
 		self.set_size((self.width, -1))
 		self.set_bg(wal.WHITE)
 
 
-	def on_paint(self, event):
+	def paint(self):
 		if not self.colors:return
 		cms = self.app.default_cms
 
@@ -48,44 +51,25 @@ class ScrolledPalette(wal.ScrolledPanel):
 		self.set_virtual_size((self.width - self.sb_width, self.height))
 		self.set_scroll_rate(self.cell_width - 1, self.cell_height - 1)
 
-		pdc = wx.PaintDC(self)
-		try:
-			dc = wx.GCDC(self.pdc)
-		except:dc = pdc
-		self.PrepareDC(dc)
-		dc.BeginDrawing()
-		pdc.BeginDrawing()
+		self.prepare_dc(self.pdc)
 
 		w = self.cell_width
 		h = self.cell_height
 		y = 1
 		x = 1
 		for color in self.colors:
-			pdc.SetPen(wx.Pen(wx.Colour(0, 0, 0), 1))
-			clr = cms.get_display_color255(color)
-			pdc.SetBrush(wx.Brush(wx.Colour(*clr)))
-			pdc.DrawRectangle(x, y, w, h)
+			self.set_stroke(wal.BLACK)
+			self.set_fill(cms.get_display_color255(color))
+			self.draw_rect(x, y, w, h)
 			x += w - 1
 			if x > (w - 1) * self.cell_in_line:
 				x = 1
 				y += h - 1
 
-			color = wal.UI_COLORS['dark_shadow']
-			dc.SetPen(wx.TRANSPARENT_PEN)
-			dc.SetBrush(wx.Brush(wx.Colour(*color)))
-			dc.DrawRectangle(self.width - self.sb_width, 0,
-							self.sb_width, self.get_size()[1])
-			color = wal.UI_COLORS['bg']
-			dc.SetBrush(wx.Brush(wx.Colour(*color)))
-			dc.DrawRectangle(self.width - self.sb_width + 1, 0,
-							self.sb_width, self.get_size()[1])
-
-		if not pdc == dc:
-			dc.EndDrawing()
-			pdc.EndDrawing()
-		else:
-			dc.EndDrawing()
-		pdc = dc = None
+		self.set_stroke(wal.UI_COLORS['dark_shadow'])
+		self.set_fill(wal.UI_COLORS['bg'])
+		self.draw_rect(self.width - self.sb_width, 0,
+						self.sb_width + 1, self.get_size()[1] + 2)
 
 class PaletteViewer(wal.VPanel):
 
@@ -94,6 +78,8 @@ class PaletteViewer(wal.VPanel):
 	def __init__(self, app, parent, palette=None):
 		self.app = app
 		wal.VPanel.__init__(self, parent, border=True)
+		options = wal.ExpandedPanel(self, _('Palette preview:'))
+		self.pack(options, fill=True)
 		self.win = ScrolledPalette(self.app, self)
 		self.pack(self.win, expand=True, fill=True)
 		if palette: self.draw_palette(palette)
