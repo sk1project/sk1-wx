2a53d925086bc5cbfe9ff89c121a257595d00af5
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-05-15 15:42:52 +0000

    palette translation to SK2 format

--- src/uc2/formats/skp/skp_presenter.py
+++ src/uc2/formats/skp/skp_presenter.py
@@ -17,11 +17,14 @@
 
 import os
 
+from copy import deepcopy
+
 from uc2 import uc2const
 from uc2.formats.generic import TextModelPresenter
 from uc2.formats.skp.skp_config import SKP_Config
 from uc2.formats.skp.skp_filters import SKP_Loader, SKP_Saver
 from uc2.formats.skp.skp_model import SK1Palette
+from uc2.formats.sk2 import sk2_model, sk2_const
 
 def create_new_palette(config):pass
 
@@ -59,5 +62,58 @@ class SKP_Presenter(TextModelPresenter):
 		pass
 
 	def translate_to_sk2(self, sk2_doc):
-		pass
+		ncells = len(self.model.colors)
+		if not ncells: return
+		cellsize = self.config.large_cell
+		if ncells > self.config.short_palette_size:
+			cellsize = self.config.small_cell
+		columns = int(self.config.palette_width / cellsize)
+		rows = 1.0 * ncells / columns
+		if rows > int(rows):
+			rows = int(rows) + 1
+		else:
+			rows = int(rows)
+
+		ext = uc2const.FORMAT_EXTENSION[uc2const.SK2][0]
+		sk2_doc.doc_file = self.model.name + "." + ext
+		sk2_doc.methods.set_doc_origin(sk2_const.DOC_ORIGIN_LU)
+		sk2_doc.methods.set_doc_units(uc2const.UNIT_PX)
+		page = sk2_doc.methods.get_page()
+
+		orient = uc2const.PORTRAIT
+		w = columns * cellsize + 1
+		h = rows * cellsize + 1
+		if w > h:orient = uc2const.LANDSCAPE
+		sk2_doc.methods.set_page_format(page, ['Custom', (w, h), orient])
+		sk2_doc.methods.set_default_page_format(['Custom', (w, h), orient])
+		grid_layer = sk2_doc.methods.get_gird_layer()
+		grid_layer.grid = [0, 0, uc2const.px_to_pt, uc2const.px_to_pt]
+		grid_layer.properties = [1, 0, 0]
+
+		layer = sk2_doc.methods.get_layer(page)
+		cfg = sk2_doc.config
+
+		x = x_orign = -(w / 2.0) + 0.5 - cellsize
+		y = h / 2.0 - 0.5
+		stroke = deepcopy(cfg.default_stroke)
+		stroke[1] = 1.0
+		index = 0
+
+		for row in range(rows):
+			y -= cellsize
+			x = x_orign
+			for column in range(columns):
+				x += cellsize
+				trafo = [cellsize, 0, 0, cellsize, x, y]
+				color = deepcopy(self.model.colors[index])
+				fill = [sk2_const.FILL_EVENODD, sk2_const.FILL_SOLID, color]
+				style = [fill, deepcopy(stroke), [], []]
+				layer.childs.append(sk2_model.Rectangle(cfg, layer,
+											trafo=trafo, style=style))
+				index += 1
+				if index == ncells: break
+
+		sk2_doc.update()
+
+
 
