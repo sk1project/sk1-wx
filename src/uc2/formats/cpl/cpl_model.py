30fe8f0eee68374530402f3689c639be14ee2321
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-05-30 16:50:56 +0000

    CPL12 format parsing

--- src/uc2/formats/cpl/cpl_model.py
+++ src/uc2/formats/cpl/cpl_model.py
@@ -15,6 +15,7 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import struct
 
 from uc2.formats.generic import BinaryModelObject
 from uc2.formats.cpl import cpl_const
@@ -29,16 +30,17 @@ class CPL7_Palette(BinaryModelObject):
 
 	version = cpl_const.CPL7
 	name = ''
+	ncolors = 0
 
 	def __init__(self):
 		self.childs = []
 		self.cache_fields = []
 
 	def parse(self, loader):
-		ncolors = loader.readword()
+		self.ncolors = loader.readword()
 		loader.fileptr.seek(0)
 		self.chunk = loader.readbytes(4)
-		for i in range(ncolors):
+		for i in range(self.ncolors):
 			color = CPL7_Color()
 			color.parse(loader)
 			self.childs.append(color)
@@ -96,6 +98,7 @@ class CPL8_Palette(BinaryModelObject):
 
 	version = cpl_const.CPL8
 	name = ''
+	ncolors = 0
 
 	def __init__(self):
 		self.childs = []
@@ -104,10 +107,10 @@ class CPL8_Palette(BinaryModelObject):
 	def parse(self, loader):
 		size = loader.readbyte()
 		self.name = loader.readstr(size)
-		ncolors = loader.readword()
+		self.ncolors = loader.readword()
 		loader.fileptr.seek(0)
 		self.chunk = loader.readbytes(2 + 1 + size + 2)
-		for i in range(ncolors):
+		for i in range(self.ncolors):
 			color = CPL8_Color()
 			color.parse(loader)
 			self.childs.append(color)
@@ -145,6 +148,9 @@ class CPL12_Palette(BinaryModelObject):
 	nheaders = 0
 	headers = {}
 	name = ''
+	palette_type = 0
+	ncolors = 0
+
 
 	def __init__(self):
 		self.childs = []
@@ -152,23 +158,91 @@ class CPL12_Palette(BinaryModelObject):
 
 	def parse(self, loader):
 		self.nheaders = loader.readdword()
+		chunk_size = 2 + 4
+		chunk_size += self.nheaders * 8
+
 		self.headers = {}
 		for i in range(self.nheaders):
 			hid, offset = loader.read_pair_dword()
 			self.headers[hid] = offset
 
+		#Palette name (header 0)
+		loader.fileptr.seek(self.headers[0], 0)
+		name_size = loader.readbyte()
+		self.name = loader.readbytes(name_size * 2).decode('utf_16_le')
+		chunk_size += 1 + name_size * 2
+
+		#Palette type (header 1)
+		loader.fileptr.seek(self.headers[1], 0)
+		self.palette_type = loader.readword()
+		chunk_size += 2
+
+		#Number of colors (header 2)
+		loader.fileptr.seek(self.headers[2], 0)
+		self.ncolors = loader.readword()
+		chunk_size += 2
+
+# TODO: data for structural object in the palette end(?)
+#		if 3 in self.headers:
+#			loader.fileptr.seek(self.headers[3], 0)
+#			ninks = loader.readword()
+#
+#		if 4 in self.headers:
+#			loader.fileptr.seek(self.headers[4], 0)
+#			size = self.headers[5] - self.headers[4]
+#			unknown = loader.readbytes()
+#
+#		if 5 in self.headers:
+#			loader.fileptr.seek(self.headers[5], 0)
+#			cols, rows = struct.unpack('<2H', loader.readbytes(4))
+
+
 		loader.fileptr.seek(0)
-		size = self.nheaders * 8
-		self.chunk = loader.readbytes(2 + 4 + size)
+		self.chunk = loader.readbytes(chunk_size)
+
+		for i in range(self.ncolors):
+			color = CPL12_Color()
+			color.parse(loader)
+			self.childs.append(color)
+
 
 	def update_for_sword(self):
 		self.cache_fields.append((0, 2, 'version'))
 		self.cache_fields.append((2, 4, 'number of headers'))
 		size = self.nheaders * 8
 		self.cache_fields.append((6, size, 'header offsets'))
+		self.cache_fields.append((self.headers[0], 1, 'palette name lenght'))
+		size = len(self.name) * 2
+		self.cache_fields.append((self.headers[0] + 1, size, 'palette name'))
+		self.cache_fields.append((self.headers[1], 2, 'palette type'))
+		self.cache_fields.append((self.headers[2], 2, 'number of colors'))
+
 
 	def resolve(self, name=''):
 		is_leaf = False
 		info = '%d' % (len(self.childs))
 		name = 'CPL12 Palette'
 		return (is_leaf, name, info)
+
+class CPL12_Color(CPL7_Color):
+	"""
+	Represents CPL12 palette color object.
+	Color values stored in valbytes field.
+	"""
+	def __init__(self):
+		CPL7_Color.__init__(self)
+
+	def parse(self, loader):
+		self.model = loader.readword()
+		self.valbytes = loader.readbytes(10)
+		size = loader.readbyte()
+		self.name = loader.readstr(size * 2).decode('utf_16_le')
+		ln = 2 + 10 + 1 + size * 2
+		loader.fileptr.seek(-ln, 1)
+		self.chunk = loader.readbytes(ln)
+
+	def update_for_sword(self):
+		self.cache_fields.append((0, 2, 'color model'))
+		self.cache_fields.append((2, 10, 'color values'))
+		self.cache_fields.append((12, 1, 'color name size'))
+		self.cache_fields.append((13, len(self.name) * 2, 'color name'))
