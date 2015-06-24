e0482a1464f00fbeeab1bb3ee2abef56c8e7e96c
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-06-24 15:19:12 +0000

    ruler configs

--- src/sk1/app_conf.py
+++ src/sk1/app_conf.py
@@ -148,11 +148,6 @@ class AppConfig(UCConfig):
 	mouse_scroll_sensitivity = 3.0
 
 	#============== RULER OPTIONS ================
-	ruler_style = 0
-	ruler_min_tick_step = 3
-	ruler_min_text_step = 50
-	ruler_max_text_step = 100
-
 	ruler_size = 20
 	ruler_font_size = 5
 	ruler_text_vshift = 3
