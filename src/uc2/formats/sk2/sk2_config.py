98db1875d0eee3972dc53d63fa8d935df17e7ec7
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2015-07-26 15:30:10 +0000

    default miter limit fix

--- src/uc2/formats/sk2/sk2_config.py
+++ src/uc2/formats/sk2/sk2_config.py
@@ -56,8 +56,7 @@ class SK2_Config(XmlConfigParser):
 	default_stroke_dash = []
 	default_stroke_cap = sk2_const.CAP_BUTT
 	default_stroke_join = sk2_const.JOIN_MITER
-	default_stroke_miter_angle = 45.0
-	default_stroke_miter_limit = 1 / math.sin(default_stroke_miter_angle / 2.0)
+	default_stroke_miter_limit = 1.0 / math.sin(math.pi / 4.0)
 	default_stroke_behind_flag = 0
 	default_stroke_scalable_flag = 0
 	default_stroke_markers = []
