966e0c35987e4089d3290719a6fca47a8f69f4ad
sK1 Project <igor.e.novikov@gmail.com>
2015-10-10 18:42:38 +0300

    proxy stubs for node connection types

--- src/sk1/app_proxy.py
+++ src/sk1/app_proxy.py
@@ -465,6 +465,10 @@ class AppProxy:
 		if canvas.mode == modes.BEZIER_EDITOR_MODE:
 			canvas.controller.convert_to_curve()
 
+	def make_node_cusp(self):pass
+	def make_node_smooth(self):pass
+	def make_node_symmetrical(self):pass
+
 	def conv_to_cmyk(self):self.app.current_doc.api.convert_bitmap(uc2const.IMAGE_CMYK)
 	def conv_to_rgb(self):self.app.current_doc.api.convert_bitmap(uc2const.IMAGE_RGB)
 	def conv_to_lab(self):self.app.current_doc.api.convert_bitmap(uc2const.IMAGE_LAB)
