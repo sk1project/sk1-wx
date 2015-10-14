6b8aff9ab9759bc01d2700314c72d7b6e9d212eb
sK1 Project <igor.e.novikov@gmail.com>
2015-10-14 15:56:16 +0300

    ActionCtxPlugin usage

--- src/sk1/context/bezier.py
+++ src/sk1/context/bezier.py
@@ -15,11 +15,8 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from wal import LEFT, CENTER, VLine, ALL, EXPAND
-
 from sk1.resources import pdids
-from sk1.pwidgets import ActionButton
-from generic import CtxPlugin
+from generic import ActionCtxPlugin
 
 BEZIER_ACTIONS = [
 pdids.ID_BEZIER_SEL_ALL_NODES, pdids.ID_BEZIER_REVERSE_ALL_PATHS, None,
@@ -32,20 +29,10 @@ pdids.ID_BEZIER_SEG_TO_LINE, pdids.ID_BEZIER_SEG_TO_CURVE, None,
 pdids.ID_BEZIER_NODE_CUSP, pdids.ID_BEZIER_NODE_SMOOTH,
 pdids.ID_BEZIER_NODE_SYMMETRICAL, ]
 
-class BezierPlugin(CtxPlugin):
+class BezierPlugin(ActionCtxPlugin):
 
 	name = 'BezierPlugin'
-
-	def __init__(self, app, parent):
-		CtxPlugin.__init__(self, app, parent)
-
-	def build(self):
-		for item in BEZIER_ACTIONS:
-			if item is None:
-				self.add(VLine(self), 0, ALL | EXPAND, 2)
-			else:
-				btn = ActionButton(self, self.actions[item])
-				self.add(btn, 0, LEFT | CENTER, 2)
+	ids = BEZIER_ACTIONS
 
 
 
