a4d93eec982ecb3883700ea094891ed0861c6eae
sK1 Project <igor.e.novikov@gmail.com>
2015-10-17 18:31:14 +0300

    rectangle editor template

---
+++ src/sk1/document/controllers/editor_rect.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# 	Copyright (C) 2015 by Igor E. Novikov
+#
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from copy import deepcopy
+
+from uc2 import libgeom
+from uc2.formats.sk2 import sk2_model
+from uc2.formats.sk2 import sk2_const
+
+from sk1 import _, modes, config, events
+from generic import AbstractController
+
+
+class RectEditor(AbstractController):
+
+	mode = modes.BEZIER_EDITOR_MODE
+	target = None
+
+	def __init__(self, canvas, presenter):
+		AbstractController.__init__(self, canvas, presenter)
