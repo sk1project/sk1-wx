f6b895a769c1c5a123466a8b58da52429ad39fd0
sK1 Project <igor.e.novikov@gmail.com>
2015-10-23 23:18:55 +0300

    polygon point implementation

--- src/sk1/document/controllers/editor_polygon.py
+++ src/sk1/document/controllers/editor_polygon.py
@@ -15,6 +15,8 @@
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import math
+
 from uc2 import libgeom
 
 from sk1 import _, modes, config, events
@@ -25,6 +27,14 @@ class PolygonEditor(AbstractController):
 	mode = modes.POLYGON_EDITOR_MODE
 	target = None
 
+	corner_points = []
+	midpoints = []
+
+	orig_angle1 = 0.0
+	orig_angle2 = 0.0
+	orig_coef1 = 1.0
+	orig_coef2 = 1.0
+
 	def __init__(self, canvas, presenter):
 		AbstractController.__init__(self, canvas, presenter)
 
@@ -33,6 +43,7 @@ class PolygonEditor(AbstractController):
 		self.target = self.selection.objs[0]
 		self.selected_obj = None
 		self.api.set_mode()
+		self.update_points()
 		self.selection.clear()
 		msg = _('Polygon in editing')
 		events.emit(events.APP_STATUS, msg)
@@ -45,6 +56,41 @@ class PolygonEditor(AbstractController):
 	def escape_pressed(self):
 		self.canvas.set_mode()
 
+	def store_props(self):
+		self.orig_angle1 = self.target.angle1
+		self.orig_angle2 = self.target.angle2
+		self.orig_coef1 = self.target.coef1
+		self.orig_coef2 = self.target.coef2
+
+	def update_points(self):
+		self.corner_points = []
+		self.midpoints = []
+		coef1 = self.target.coef1
+		coef2 = self.target.coef2
+		angle1 = self.target.angle1
+		angle2 = self.target.angle2
+		corners_num = self.target.corners_num
+		center = [0.5, 0.5]
+		corner_angle = 2.0 * math.pi / float(corners_num)
+		corners_start = [0.5, 0.5 + 0.5 * coef1]
+		midpoint_start = [0.5, 0.5 + 0.5 * coef2 * math.cos(corner_angle / 2.0)]
+
+		corner_angle_shift = angle1
+		midpoint_angle_shift = corner_angle / 2.0 + angle2
+
+		for i in range(0, corners_num):
+			angle = float(i) * corner_angle + corner_angle_shift
+			point = libgeom.rotate_point(center, corners_start, angle)
+			self.corner_points.append(CornerPoint(self.canvas,
+												self.target, point, i))
+
+			angle = float(i) * corner_angle + midpoint_angle_shift
+			point = libgeom.rotate_point(center, midpoint_start, angle)
+			self.midpoints.append(CornerPoint(self.canvas,
+												self.target, point, i))
+		msg = _('Polygon in editing')
+		events.emit(events.APP_STATUS, msg)
+
 	#----- REPAINT
 
 	def repaint(self):
@@ -52,6 +98,8 @@ class PolygonEditor(AbstractController):
 		p0 = self.canvas.point_doc_to_win([x0, y0])
 		p1 = self.canvas.point_doc_to_win([x1, y1])
 		self.canvas.renderer.draw_frame(p0, p1)
+		for item in self.corner_points: item.repaint()
+		for item in self.midpoints: item.repaint()
 
 	#----- MOUSE CONTROLLING
 	def mouse_down(self, event):
@@ -67,3 +115,29 @@ class PolygonEditor(AbstractController):
 			self.canvas.set_mode(modes.SHAPER_MODE)
 
 	def mouse_move(self, event):pass
+
+
+class CornerPoint:
+
+	canvas = None
+	target = None
+	point = []
+
+	def __init__(self, canvas, target, point, index):
+		self.canvas = canvas
+		self.target = target
+		self.point = point
+
+	def get_point(self):
+		return libgeom.apply_trafo_to_point(self.point, self.target.trafo)
+
+	def get_screen_point(self):
+		return self.canvas.point_doc_to_win(self.get_point())
+
+	def is_pressed(self, win_point):
+		wpoint = self.canvas.point_doc_to_win(self.get_point())
+		bbox = libgeom.bbox_for_point(wpoint, config.point_sensitivity_size)
+		return libgeom.is_point_in_bbox(win_point, bbox)
+
+	def repaint(self):
+		self.canvas.renderer.draw_polygon_point(self.get_screen_point())
