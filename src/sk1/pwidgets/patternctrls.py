86c0ab5f09a1aa77e43158c75e726199f02d8abe
sK1 Project <igor.e.novikov@gmail.com>
2015-10-29 16:24:34 +0200

    PatternPalete() object

--- src/sk1/pwidgets/patternctrls.py
+++ src/sk1/pwidgets/patternctrls.py
@@ -20,3 +20,52 @@ import wal
 
 from uc2 import uc2const
 from uc2.formats.sk2 import sk2_const
+
+from colorctrls import SwatchCanvas
+from patterns import PATTERN_PRESETS
+
+class PatternPaletteSwatch(wal.VPanel, SwatchCanvas):
+
+	callback = None
+	pattern = None
+
+	def __init__(self, parent, cms, pattern, size=(40, 20), onclick=None):
+		self.color = None
+		self.cms = cms
+		wal.VPanel.__init__(self, parent)
+		SwatchCanvas.__init__(self)
+		self.pack(size)
+		if onclick: self.callback = onclick
+		self.set_pattern(pattern)
+
+	def set_pattern(self, pattern):
+		self.pattern = pattern
+		self.fill = [0, sk2_const.FILL_PATTERN,
+				[sk2_const.PATTERN_IMG, pattern]]
+		self.refresh()
+
+	def mouse_left_up(self, point):
+		if self.callback: self.callback(deepcopy(self.pattern))
+
+class PatternMiniPalette(wal.VPanel):
+
+	callback = None
+	cells = []
+
+	def __init__(self, parent, cms, stops=[], onclick=None):
+		wal.VPanel.__init__(self, parent)
+		self.set_bg(wal.BLACK)
+		grid = wal.GridPanel(parent, 2, 6, 1, 1)
+		grid.set_bg(wal.BLACK)
+		self.cells = []
+
+		for item in range(12):
+			print item
+			self.cells.append(PatternPaletteSwatch(grid, cms,
+							PATTERN_PRESETS[item], onclick=self.on_click))
+			grid.pack(self.cells[-1])
+		self.pack(grid, padding_all=1)
+		if onclick: self.callback = onclick
+
+	def on_click(self, pattern):
+		if self.callback: self.callback(pattern)
