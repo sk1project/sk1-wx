58b7c5a49150b244b657b7d808377c1af93f59a9
sK1 Project <igor.e.novikov@gmail.com>
2015-11-09 18:45:12 +0200

    OrientationIndicator usage, event connection

--- src/sk1/share/pd_plugins/transform_plugin/__init__.py
+++ src/sk1/share/pd_plugins/transform_plugin/__init__.py
@@ -18,9 +18,11 @@
 
 import os, wal
 
-from sk1 import _
+from sk1 import _, events
 from sk1.app_plugins import RS_Plugin
 from sk1.resources import get_icon
+
+from indicator import OrientationIndicator
 
 PLG_DIR = __path__[0]
 IMG_DIR = os.path.join(PLG_DIR, 'images')
@@ -72,15 +74,32 @@ class Transform_Plugin(RS_Plugin):
 		panel.pack(self.transform_keeper)
 		panel.pack(wal.HLine(panel), fill=True, padding=3)
 
-		panel.pack(wal.Button(panel, _('Apply to copy'),
-							onclick=self.action_copy), fill=True)
-		panel.pack(wal.Button(panel, _('Apply'), onclick=self.action),
-				padding=3, fill=True)
+		panel.pack(OrientationIndicator(panel), padding_all=10)
+
+		self.apply_copy_btn = wal.Button(panel, _('Apply to copy'),
+							onclick=self.action_copy)
+		panel.pack(self.apply_copy_btn, fill=True)
+
+		self.apply_btn = wal.Button(panel, _('Apply'), onclick=self.action)
+		panel.pack(self.apply_btn, padding=3, fill=True)
 
 		panel.pack(wal.HLine(panel), fill=True)
 
 		self.panel.pack(panel, fill=True, expand=True, padding_all=5)
 
+		events.connect(events.DOC_CHANGED, self.update)
+		events.connect(events.SELECTION_CHANGED, self.update)
+		self.update()
+
 	def on_mode_change(self, mode):pass
 	def action_copy(self): self.action(True)
 	def action(self, copy=False):pass
+
+	def update(self, *args):
+		state = False
+		if self.app.insp.is_selection(): state = True
+		self.apply_btn.set_enable(state)
+		self.apply_copy_btn.set_enable(state)
+
+	def show_signal(self, mode=POSITION_MODE, *args):
+		self.transform_keeper.set_mode(mode)
