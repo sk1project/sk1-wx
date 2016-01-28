4d4343de0bca868b6e2c60a51cbb6ab51f082c9b
sK1 Project <igor.e.novikov@gmail.com>
2016-01-28 19:27:39 +0200

    LayerList test

--- ui-tests/ulist-test.py
+++ ui-tests/ulist-test.py
@@ -1,19 +1,31 @@
 import wal
 
-TEST_ULIST = [
+TEST_LIST = [
 		[0, 1, 1, 1, 'Layer3a'],
 		[0, 1, 1, 1, 'Layer3b'],
-		[1, 1, 1, 1, 'Layer3c'],
-		[0, 1, 1, 1, 'Layer3d'],
+		[1, 1, 0, 1, 'Layer3c'],
+		[0, 1, 1, 0, 'Layer3d'],
 		[0, 1, 1, 1, 'Layer3f'],
+]
+
+BITMAPS = [
+		'gtk-justify-fill', 'gtk-yes',
+		'gtk-stop', 'gtk-index',
+		'gtk-stop', 'gtk-edit',
+		'gtk-print-error', 'gtk-print',
 ]
 
 class WidgetPanel(wal.HPanel):
 	def __init__(self, parent):
 		wal.HPanel.__init__(self, parent)
+		pnl = wal.VPanel(self, border=True)
+		lst = wal.LayerList(pnl, TEST_LIST, BITMAPS)
+		pnl.pack(lst, fill=True, expand=True)
+		self.pack(pnl, padding_all=10, fill=True, expand=True)
+
 
 app = wal.Application('wxWidgets')
-mw = wal.MainWindow('UList widget', (210, 300))
+mw = wal.MainWindow('LayerList widget', (210, 300))
 panel = WidgetPanel(mw)
 mw.pack(panel, expand=True, fill=True, padding=10)
 app.mw = mw
