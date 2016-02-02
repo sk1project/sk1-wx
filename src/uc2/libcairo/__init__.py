8c59358abe62b4153df5c52bd059f87127a1cab4
sK1 Project <igor.e.novikov@gmail.com>
2016-02-02 20:01:27 +0200

    Pango related functionality refactored into libpango

--- src/uc2/libcairo/__init__.py
+++ src/uc2/libcairo/__init__.py
@@ -16,38 +16,13 @@
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import cairo
-#import pangocairo
 import _libcairo
 
 SURFACE = cairo.ImageSurface(cairo.FORMAT_RGB24, 1, 1)
 CTX = cairo.Context(SURFACE)
 DIRECT_MATRIX = cairo.Matrix()
 
-PANGO_MATRIX = cairo.Matrix(1.0, 0.0, 0.0, -1.0, 0.0, 0.0)
-#PCCTX = pangocairo.CairoContext(CTX)
-#PANGO_LAYOUT = PCCTX.create_layout()
 
-FAMILIES_LIST = []
-FAMILIES_DICT = {}
-
-#def get_fonts(families_list, families_dict):
-#	fm = pangocairo.cairo_font_map_get_default()
-#	context = fm.create_context()
-#	families = context.list_families()
-#	for item in families:
-#		fcs = []
-#		scalable = True
-#		for face in item.list_faces():
-#			if not face.list_sizes() is None:
-#				scalable = False
-#			fcs.append(face.get_face_name())
-#		if scalable:
-#			fcs.sort()
-#			families_dict[item.get_name()] = fcs
-#			families_list.append(item.get_name())
-#	families_list.sort()
-#
-#get_fonts(FAMILIES_LIST, FAMILIES_DICT)
 
 def get_version():
 	cairo_ver = cairo.cairo_version_string()
