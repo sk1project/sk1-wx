2085a4ebecb31b5e7b1501159f521483c65ae074
sK1 Project <igor.e.novikov@gmail.com>
2016-03-23 14:11:33 +0200

    minor changes

--- src/sk1/document/controllers/text_ctrl.py
+++ src/sk1/document/controllers/text_ctrl.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# 	Copyright (C) 2017 by Igor E. Novikov
+# 	Copyright (C) 2016 by Igor E. Novikov
 #
 # 	This program is free software: you can redistribute it and/or modify
 # 	it under the terms of the GNU General Public License as published by
