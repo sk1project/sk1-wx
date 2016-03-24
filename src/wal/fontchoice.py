759ed1c78b6df675ce7f2db37ae2cde4115838e1
sK1 Project <igor.e.novikov@gmail.com>
2016-03-24 16:43:44 +0200

    FontChoice UI improvement

--- src/wal/fontchoice.py
+++ src/wal/fontchoice.py
@@ -62,7 +62,8 @@ class FontBitmapChoice(wx.combo.OwnerDrawnComboBox, Widget):
 		label_x = icon_x + 4 + icon_size[0]
 		icon_y = self.control_height / 2 - icon_size[1] / 2 + label_y
 		sample_y = label_y + self.control_height + 2
-		if flags & wx.combo.ODCB_PAINTING_SELECTED:
+		if flags & wx.combo.ODCB_PAINTING_SELECTED and not \
+		flags & wx.combo.ODCB_PAINTING_CONTROL:
 			if self.font_icon:
 				icon = bmp_to_white(self.font_icon)
 				dc.DrawBitmap(icon, icon_x, icon_y, True)
@@ -71,6 +72,17 @@ class FontBitmapChoice(wx.combo.OwnerDrawnComboBox, Widget):
 			bmp = bmp_to_white(self.sample_bitmaps[item])
 			dc.DrawBitmap(bmp, label_x, sample_y, True)
 		elif flags & wx.combo.ODCB_PAINTING_CONTROL:
+			if const.is_gtk():
+				icon_x += 2
+				label_x += 2
+				pdc = wx.PaintDC(self)
+				pdc.SetPen(wx.TRANSPARENT_PEN)
+				pdc.SetBrush(wx.Brush(wx.Colour(*self.GetBackgroundColour())))
+				h = self.get_size()[1]
+				w = r.width + 2
+				pdc.DrawRectangle(0, 0, w, h)
+				nr = wx.RendererNative.Get()
+				nr.DrawTextCtrl(self, dc, (0, 0, w, h), wx.CONTROL_DIRTY)
 			if self.font_icon:
 				dc.DrawBitmap(self.font_icon, icon_x, icon_y, True)
 			dc.DrawBitmap(self.bitmaps[item], label_x, label_y, True)
