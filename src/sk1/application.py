85f3284fb388e5ccba5fd250e9377e3edd2ca111
sK1 Project <igor.e.novikov@gmail.com>
2016-04-20 15:42:20 +0300

    refactoring

--- src/sk1/application.py
+++ src/sk1/application.py
@@ -15,7 +15,7 @@
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys, traceback, time
+import os, sys, traceback
 import webbrowser
 from base64 import b64decode
 
