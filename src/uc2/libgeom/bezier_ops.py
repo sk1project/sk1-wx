2560b44cc94c3e4ecfae2b11f88e13dd7ad9a5d3
sK1 Project <igor.e.novikov@gmail.com>
2016-04-20 15:40:32 +0300

    refactoring

--- src/uc2/libgeom/bezier_ops.py
+++ src/uc2/libgeom/bezier_ops.py
@@ -15,7 +15,6 @@
 #	You should have received a copy of the GNU General Public License
 #	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import math
 from copy import deepcopy
 
 from uc2.formats.sk2 import sk2_const
