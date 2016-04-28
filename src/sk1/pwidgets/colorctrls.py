60b2f8b2b163cedc34d1e58f1cc5537738e5a8a5
sK1 Project <igor.e.novikov@gmail.com>
2016-04-28 11:27:14 +0300

    msw specific layout fixes

--- src/sk1/pwidgets/colorctrls.py
+++ src/sk1/pwidgets/colorctrls.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2015 by Igor E. Novikov
+# 	Copyright (C) 2015 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import cairo, wal, os
 from copy import deepcopy
@@ -731,7 +731,7 @@ class CMYK_Mixer(wal.GridPanel):
 	callback = None
 
 	def __init__(self, parent, cms, color=None, onchange=None):
-		wal.GridPanel.__init__(self, parent, 4, 4, 3, 5)
+		wal.GridPanel.__init__(self, parent, 5, 4, 3, 5)
 		self.cms = cms
 		if color:
 			self.color = color
