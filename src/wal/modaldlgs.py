2305157938624591e2a25f6e4a4a5285251dd79e
sK1 Project <igor.e.novikov@gmail.com>
2016-04-28 11:31:32 +0300

    msw specific layout fix

--- src/wal/modaldlgs.py
+++ src/wal/modaldlgs.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2013-2015 by Igor E. Novikov
+# 	Copyright (C) 2013-2015 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import wx
 
@@ -55,6 +55,7 @@ class SimpleDialog(wx.Dialog):
 		self.set_dialog_buttons()
 		if size == (-1, -1):self.Fit()
 		self.CenterOnParent()
+		self.panel.layout()
 		self.Bind(wx.EVT_CLOSE, self.on_close, self)
 		if on_load:
 			self._timer = wx.Timer(self)
