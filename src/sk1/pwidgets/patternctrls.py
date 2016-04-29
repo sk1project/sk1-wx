cde3257bd36cda1fe6503f7152e6b5c652b658f5
sK1 Project <igor.e.novikov@gmail.com>
2016-04-29 19:46:40 +0300

    degrees sign replaced

--- src/sk1/pwidgets/patternctrls.py
+++ src/sk1/pwidgets/patternctrls.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2015 by Igor E. Novikov
+# 	Copyright (C) 2015 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import math
 from copy import deepcopy
@@ -194,7 +194,7 @@ class TransformEditor(wal.VPanel):
 						step=1.0, width=5,
 						onchange=self.changes, onenter=self.changes)
 		grid.pack(self.shear_x)
-		grid.pack(wal.Label(grid, '°'))
+		grid.pack(wal.Label(grid, _('deg')))
 
 		grid.pack((10, 5))
 
@@ -205,7 +205,7 @@ class TransformEditor(wal.VPanel):
 						step=1.0, width=5,
 						onchange=self.changes, onenter=self.changes)
 		grid.pack(self.shear_y)
-		grid.pack(wal.Label(grid, '°'))
+		grid.pack(wal.Label(grid, _('deg')))
 
 		self.pack(grid)
 
