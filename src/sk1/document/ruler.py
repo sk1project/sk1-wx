24375923f5acc32fd4b613a9497903a0f4169b8d
sK1 Project <igor.e.novikov@gmail.com>
2016-05-01 15:26:29 +0300

    msw specific improvement

--- src/sk1/document/ruler.py
+++ src/sk1/document/ruler.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2013 by Igor E. Novikov
+# 	Copyright (C) 2013 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os, math
-import wx
+import wx, wal
 import cairo
 
 from uc2 import uc2const, cms
@@ -165,6 +165,7 @@ class Ruler(HPanel):
 		self.Bind(wx.EVT_MOUSE_CAPTURE_LOST, self.capture_lost)
 		self.eventloop.connect(self.eventloop.VIEW_CHANGED, self.repaint)
 		events.connect(events.CONFIG_MODIFIED, self.check_config)
+		if wal.is_msw(): self.SetDoubleBuffered(True)
 		if is_mac():
 			self.timer = wx.Timer(self)
 			self.Bind(wx.EVT_TIMER, self._repaint_after)
