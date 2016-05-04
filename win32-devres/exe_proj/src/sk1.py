f1bc2336087df531dbb3baaaa66f16263ca02e5e
sK1 Project <igor.e.novikov@gmail.com>
2016-05-04 15:57:52 +0300

    exe_proj moved

---
+++ win32-devres/exe_proj/src/sk1.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# 	Copyright (C) 2016 by Igor E. Novikov
+#
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os, sys
+
+cur_path = os.getcwd()
+bindir = os.path.join(cur_path, 'dlls') + os.pathsep
+magickdir = os.path.join(cur_path, 'dlls', 'modules') + os.pathsep
+
+
+os.environ["PATH"] = magickdir + bindir + os.environ["PATH"]
+os.environ["MAGICK_CODER_MODULE_PATH"] = magickdir
+os.environ["MAGICK_CODER_FILTER_PATH"] = magickdir
+os.environ["MAGICK_CONFIGURE_PATH"] = magickdir
+os.environ["MAGICK_HOME"] = magickdir
+
+libs_path = os.path.join(cur_path, 'libs')
+sys.path.insert(0, libs_path)
+
+import sk1
+
+sk1.sk1_run()
