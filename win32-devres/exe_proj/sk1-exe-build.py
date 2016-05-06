2695ce468ebc235f3fd89a6b4053aad7ff6ce5ca
sK1 Project <igor.e.novikov@gmail.com>
2016-05-06 16:51:12 +0300

    exe build fixes

--- win32-devres/exe_proj/sk1-exe-build.py
+++ win32-devres/exe_proj/sk1-exe-build.py
@@ -6,16 +6,21 @@ if len(sys.argv) == 1:
 from distutils.core import setup
 import py2exe
 
+from glob import glob
+data_files = [("Microsoft.VC90.CRT",
+glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]
 
 setup(
-	options={'py2exe': {'bundle_files': 3,
+	options={'py2exe': {'bundle_files': 2,
 						'compressed': True,
 'includes':['base64', 'cgi', 'colorsys', 'copy', 'datetime', 'errno', 'inspect',
 'math', 'new', 'numbers', 'os', 'platform', 'shutil', 'StringIO', 'cStringIO',
-'struct', 'sys', 'tempfile', 'time', 'traceback', 'types',
+'struct', 'sys', 'tempfile', 'time', 'traceback', 'types', 'fractions',
 'unicodedata', 'urllib2', 'webbrowser', 'xml.sax', 'zipfile', 'zlib', ],
 						}},
 	windows=[{'script': "src\\sk1.py",
 			"icon_resources": [(1, "src\\sk1.ico")]
 			}],
+	data_files=data_files,
+	zipfile=None,
 	)
