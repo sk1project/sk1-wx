6f4c37ea6ada42cb2443bf8369fe33036a8ffd66
sK1 Project <igor.e.novikov@gmail.com>
2016-05-11 18:32:11 +0300

    working script

---
+++ win64-devres/def_generator.py
@@ -0,0 +1,24 @@
+import os
+
+names = []
+
+MSVCLIBS = ['mfc120u.dll',
+'msvcp120.dll',
+'msvcr90.dll',
+'msvcr120.dll',
+'vcomp120.dll', ]
+
+abspath = os.path.abspath('dlls')
+
+for item in os.listdir('dlls'):
+    item = str(item)
+    if not item[:4] == 'CORE' and not item in MSVCLIBS and not item == 'modules':
+        names.append(item)
+
+os.mkdir('defs')
+absdefpath = os.path.abspath('defs')
+
+for item in names:
+    dllname = os.path.join(abspath, item)
+    defname = os.path.join(absdefpath, item[:-4] + '.def')
+    os.system('dumpbin /exports %s >%s' % (dllname, defname))
