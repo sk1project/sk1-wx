78aa1f70bc4204ef9cce70fc44cf6abf45ddda68
sK1 Project <igor.e.novikov@gmail.com>
2016-05-12 14:30:22 +0300

    lib generator scripts

---
+++ win64-devres/lib_generator.py
@@ -0,0 +1,16 @@
+import os
+
+names = []
+
+absdefpath = os.path.abspath('defs')
+
+for item in os.listdir('defs'):
+    item = str(item)
+    names.append(item)
+    
+abslibpath = os.path.abspath('libs')
+
+for item in names:
+    defname = os.path.join(absdefpath, item)
+    libname = os.path.join(abslibpath, item[:-4] + '.lib')
+    os.system('LIB /NOLOGO /MACHINE:X64 /def:%s /out:%s' % (defname, libname))
