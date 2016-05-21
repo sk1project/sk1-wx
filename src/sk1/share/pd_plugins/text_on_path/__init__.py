956bf523c994e4d082c551a4494f74fe9e139770
sK1 Project <igor.e.novikov@gmail.com>
2016-05-21 12:37:59 +0300

    Text-on-Path ui implementation

--- src/sk1/share/pd_plugins/text_on_path/__init__.py
+++ src/sk1/share/pd_plugins/text_on_path/__init__.py
@@ -22,7 +22,7 @@ from uc2.formats.sk2 import sk2_const
 
 from sk1 import _, events
 from sk1.app_plugins import RS_Plugin
-from sk1.resources import get_icon
+from sk1.resources import get_icon, icons, get_bmp
 
 PLG_DIR = __path__[0]
 IMG_DIR = os.path.join(PLG_DIR, 'images')
@@ -35,6 +35,27 @@ def get_plugin(app):
 
 PLUGIN_ICON = make_artid('icon')
 
+TEXT_ALIGNS = [sk2_const.TEXT_ALIGN_LEFT, sk2_const.TEXT_ALIGN_CENTER,
+			sk2_const.TEXT_ALIGN_RIGHT, sk2_const.TEXT_ALIGN_JUSTIFY]
+
+TEXT_ALIGN_ICONS = {
+sk2_const.TEXT_ALIGN_LEFT:icons.PD_ALIGN_LEFT,
+sk2_const.TEXT_ALIGN_CENTER:icons.PD_ALIGN_CENTER,
+sk2_const.TEXT_ALIGN_RIGHT:icons.PD_ALIGN_RIGHT,
+sk2_const.TEXT_ALIGN_JUSTIFY:icons.PD_ALIGN_JUSTIFY,
+}
+
+TEXT_ALIGN_TEXTS = {
+sk2_const.TEXT_ALIGN_LEFT:_('Align to path start'),
+sk2_const.TEXT_ALIGN_CENTER:_('Align to path center'),
+sk2_const.TEXT_ALIGN_RIGHT:_('Align to path end'),
+sk2_const.TEXT_ALIGN_JUSTIFY:_('Stretch along path'),
+}
+
+TEXT_ALIGN_PICS = {
+sk2_const.TEXT_ALIGN_LEFT:make_artid('pos-start'),
+}
+
 class TP_Plugin(RS_Plugin):
 
 	pid = 'TextOnPathPlugin'
@@ -44,3 +65,39 @@ class TP_Plugin(RS_Plugin):
 
 	def build_ui(self):
 		self.icon = get_icon(PLUGIN_ICON)
+		panel = wal.VPanel(self.panel)
+
+		panel.pack(wal.Label(panel, _('Text position on path')), padding_all=5)
+
+		self.align_keeper = wal.HToggleKeeper(panel, TEXT_ALIGNS,
+							TEXT_ALIGN_ICONS,
+							TEXT_ALIGN_TEXTS)
+		panel.pack(self.align_keeper)
+		self.align_keeper.set_mode(sk2_const.TEXT_ALIGN_LEFT)
+
+		border = wal.VPanel(panel)
+		color = wal.GRAY
+		if wal.is_gtk():color = wal.UI_COLORS['pressed_border']
+		border.set_bg(color)
+		self.pic_panel = wal.VPanel(border)
+		self.pic_panel.set_bg(wal.WHITE)
+		self.bmp = get_bmp(self.pic_panel,
+						TEXT_ALIGN_PICS[sk2_const.TEXT_ALIGN_LEFT])
+		self.pic_panel.pack(self.bmp, padding_all=5)
+		border.pack(self.pic_panel, padding_all=1)
+		panel.pack(border, padding=10)
+
+		self.other_side = wal.Checkbox(panel, _('Place on other side'))
+		panel.pack(self.other_side, padding=5)
+
+		self.apply_btn = wal.Button(panel, _('Apply'), onclick=self.action)
+		panel.pack(self.apply_btn, padding=5, fill=True)
+
+		self.panel.pack(panel, fill=True, padding_all=5)
+		self.panel.pack(wal.HLine(self.panel), fill=True)
+
+		self.update()
+
+	def update(self, *args):pass
+	def action(self):pass
+
