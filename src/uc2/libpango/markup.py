932757fd286dea0730d09b7b4b797f441acc5812
sK1 Project <igor.e.novikov@gmail.com>
2016-06-24 11:17:20 +0300

    glyph specific markup

--- src/uc2/libpango/markup.py
+++ src/uc2/libpango/markup.py
@@ -37,17 +37,17 @@ def intersect_ranges(text_range, markup):
 
 def get_tags_from_descr(tag_descr, check_nt=False):
 	if not isinstance(tag_descr, tuple):
-		if tag_descr == 'sub':
+		if tag_descr in ('sub', 'sup'):
 			return '<span size="xx-small">', '</span>'
 		return '<%s>' % tag_descr, '</%s>' % tag_descr
 	#TODO:implement <span> tag
 
-def markup_to_tag_dict(markup, check_nt=False):
+def markup_to_tag_dict(markup):
 	tag_dict = {}
 	for item in markup:
 		tag_descr = item[0]
 		rng = item[1]
-		tag, tag_end = get_tags_from_descr(tag_descr, check_nt)
+		tag, tag_end = get_tags_from_descr(tag_descr)
 		if rng[0] in tag_dict:
 			tag_dict[rng[0]] = tag + tag_dict[rng[0]]
 		else:
@@ -58,19 +58,53 @@ def markup_to_tag_dict(markup, check_nt=False):
 			tag_dict[rng[1]] = tag_end
 	return tag_dict
 
-def apply_markup(text, text_range, markup, check_nt=False):
-	markup = [('sub', (2, 5)), ]
+def apply_markup(text, markup):
 	if not markup: return escape(text)
-	local_markup = intersect_ranges(text_range, markup)
+	local_markup = intersect_ranges([0, len(text)], markup)
 	if not local_markup: return escape(text)
 	text_list = []
 	for item in text:
 		text_list.append(escape(item))
-	tag_dict = markup_to_tag_dict(local_markup, check_nt)
+	tag_dict = markup_to_tag_dict(local_markup)
 	keys = tag_dict.keys()
 	keys.sort(reverse=True)
 	for index in keys:
 		text_list.insert(index, tag_dict[index])
-	print ''.join(text_list)
 	return ''.join(text_list)
+
+#--- Glyph markup
+
+def glyph_markup_to_tag_dict(markup, check_nt=False):
+	tag_dict = {}
+	vpos = 0.0
+	for item in markup:
+		tag_descr = item[0]
+		rng = item[1]
+		if tag_descr == 'sub': vpos = -0.3
+		elif tag_descr == 'sup': vpos = 0.8
+		tag, tag_end = get_tags_from_descr(tag_descr, check_nt)
+		if rng[0] in tag_dict:
+			tag_dict[rng[0]] = tag + tag_dict[rng[0]]
+		else:
+			tag_dict[rng[0]] = tag
+		if rng[1] in tag_dict:
+			tag_dict[rng[1]] += tag_end
+		else:
+			tag_dict[rng[1]] = tag_end
+	return tag_dict, vpos
+
+def apply_glyph_markup(text, text_range, markup, check_nt=False):
+	if not markup: return escape(text), 0.0
+	local_markup = intersect_ranges(text_range, markup)
+	if not local_markup: return escape(text), 0.0
+	text_list = []
+	for item in text:
+		text_list.append(escape(item))
+	tag_dict, vpos = glyph_markup_to_tag_dict(local_markup, check_nt)
+	keys = tag_dict.keys()
+	keys.sort(reverse=True)
+	for index in keys:
+		text_list.insert(index, tag_dict[index])
+	return ''.join(text_list), vpos
+
 
