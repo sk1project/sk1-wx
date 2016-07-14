a81959caf0c479dda9152c49a03a91d52326783a
sK1 Project <igor.e.novikov@gmail.com>
2016-07-14 20:02:14 +0300

    Iconizer plugin template

---
+++ src/sk1/share/pd_plugins/iconizer/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# 	Copyright (C) 2016 by Igor E. Novikov
+#
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os, wal
+
+from uc2.formats.sk2 import sk2_const
+
+from sk1 import _, events
+from sk1.app_plugins import RS_Plugin
+from sk1.resources import get_icon
+
+PLG_DIR = __path__[0]
+IMG_DIR = os.path.join(PLG_DIR, 'images')
+
+def make_artid(name):
+	return os.path.join(IMG_DIR, name + '.png')
+
+def get_plugin(app):
+	return Iconizer_Plugin(app)
+
+PLUGIN_ICON = make_artid('icon')
+
+class Iconizer_Plugin(RS_Plugin):
+
+	pid = 'IconizerPlugin'
+	name = _('Iconizer')
+	active_transform = None
+	transforms = {}
+
+	def build_ui(self):
+		self.icon = get_icon(PLUGIN_ICON)
+		self.update()
+
+	def update(self, *args):pass
