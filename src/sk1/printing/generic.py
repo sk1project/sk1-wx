21732a6370d7ebd600354c51be18a5f1b239d1e7
sK1 Project <igor.e.novikov@gmail.com>
2016-07-27 18:20:09 +0300

    generic interfaces for printing

---
+++ src/sk1/printing/generic.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# 	Copyright (C) 2016 by Igor E. Novikov
+#
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class AbstractPrinter(object):
+
+	name = 'Abstract Printer'
+
+	def get_name(self):return self.name
+
+class AbstractPS(object):
+
+	def get_default_printer(self): return None
+
+class AbstractPrintout(object):pass
