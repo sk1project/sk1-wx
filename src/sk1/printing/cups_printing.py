ecbceba1c3f1b4bb271ef671b1cfc55f490824d8
sK1 Project <igor.e.novikov@gmail.com>
2016-07-28 20:11:33 +0300

    RangePanel and CopiesPanel

--- src/sk1/printing/cups_printing.py
+++ src/sk1/printing/cups_printing.py
@@ -27,6 +27,44 @@ from sk1.resources import icons
 import prn_events
 from cups_staff import CUPS_PS, CUPS_Printout
 
+class RangePanel(wal.LabeledPanel):
+
+	def __init__(self, parent, win, printout):
+		wal.LabeledPanel.__init__(self, parent, _('Print range'))
+
+		vpanel = wal.VPanel(self)
+
+		grid = wal.GridPanel(vpanel, 3, 2, 5, 5)
+		grid.add_growable_col(1)
+		self.all_opt = wal.Radiobutton(grid, _('All'), group=True)
+		self.sel_opt = wal.Radiobutton(grid, _('Selection'))
+		self.cpage_opt = wal.Radiobutton(grid, _('Current page'))
+		self.pages_opt = wal.Radiobutton(grid, _('Pages:'))
+		self.pages_entry = wal.Entry(grid, '1')
+		self.all_opt.set_value(True)
+		grid.pack(self.all_opt)
+		grid.pack((1, 1))
+		grid.pack(self.sel_opt)
+		grid.pack(self.cpage_opt)
+		grid.pack(self.pages_opt)
+		grid.pack(self.pages_entry, fill=True)
+		vpanel.pack(grid, fill=True)
+
+		vpanel.pack((5, 5))
+
+		title = _('Enter page numbers or page ranges.')
+		title += '\n' + _('For example: 1,2,5-6')
+		vpanel.pack(wal.Label(self, title, fontsize=-1), align_center=False)
+
+		self.pack(vpanel, fill=True, padding_all=10)
+
+class CopiesPanel(wal.LabeledPanel):
+
+	def __init__(self, parent, win, printout):
+		wal.LabeledPanel.__init__(self, parent, _('Copies'))
+		title = _('Number of copies:')
+		self.pack(wal.Label(self, title))
+
 class PrinterPanel(wal.LabeledPanel):
 
 	printsys = None
@@ -114,11 +152,19 @@ class GeneralTab(wal.VPanel):
 
 	name = _('General')
 
-	def __init__(self, parent, win, printsys):
+	def __init__(self, parent, win, printsys, printout):
 		wal.VPanel.__init__(self, parent)
 
 		self.prn_panel = PrinterPanel(self, win, printsys)
 		self.pack(self.prn_panel, fill=True, padding_all=5)
+
+		hpanel = wal.HPanel(self)
+		self.range_panel = RangePanel(self, win, printout)
+		hpanel.pack(self.range_panel, fill=True, expand=True)
+		hpanel.pack((5, 5))
+		self.copies_panel = CopiesPanel(self, win, printout)
+		hpanel.pack(self.copies_panel, fill=True, expand=True)
+		self.pack(hpanel, fill=True, expand=True, padding_all=5)
 
 class CUPSPrintDialog(wal.OkCancelDialog):
 
@@ -140,7 +186,8 @@ class CUPSPrintDialog(wal.OkCancelDialog):
 	def build(self):
 		self.nb = wal.Notebook(self.panel)
 
-		self.general_tab = GeneralTab(self.nb, self, self.printsys)
+		self.general_tab = GeneralTab(self.nb, self,
+									self.printsys, self.printout)
 		self.nb.add_page(self.general_tab, self.general_tab.name)
 
 		self.layouts_tab = wal.VPanel(self.nb)
