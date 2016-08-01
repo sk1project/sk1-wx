0d4e19308109e9220f2c4fb406639f6a4ac10496
sK1 Project <igor.e.novikov@gmail.com>
2016-08-01 20:09:21 +0300

    selection and current page features, print range and page range functionality

--- src/sk1/printing/printout.py
+++ src/sk1/printing/printout.py
@@ -20,29 +20,49 @@ class PrnGroup(object):
 	childs = []
 	trafo = [1.0, 0.0, 0.0, 1.0, 0.0, 0.0]
 
+PRINT_ALL = 0
+PRINT_SELECTION = 1
+PRINT_CURRENT_PAGE = 2
+PRINT_PAGE_RANGE = 4
 
 class Printout(object):
 
 	app = None
 	doc = None
 	pages = []
-	print_pages = []
+	current_page = []
+	selection = None
+	reverse_flag = False
+	print_range = PRINT_ALL
+	page_range = []
 
 	def __init__(self, doc):
 		self.app = doc.app
 		self.doc = doc
-		self.pages = self.collect_pages(doc)
+		self.pages, self.current_page = self.collect_pages(doc)
+		if self.app.insp.is_selection():
+			self.selection = self.doc.selection.objs
 
 	def collect_pages(self, doc):
 		pages = []
+		cur_page = []
 		mtds = doc.methods
+		active_page = self.doc.active_page
 		for item in mtds.get_pages():
 			page = []
 			for layer in item.childs:
 				if mtds.is_layer_printable(layer):
 					page += layer.childs
 			pages.append(page)
-		return pages
+			if item == active_page:
+				cur_page = page
+		return pages, cur_page
 
-	def is_selection(self): return False
-	def get_num_pages(self): return 1
+	def is_selection(self): return bool(self.selection)
+	def get_num_pages(self): return len(self.pages)
+
+	def set_print_range(self, print_range, page_range=[]):
+		self.print_range = print_range
+		self.page_range = page_range
+
+	def set_reverse(self, val): self.reverse_flag = val
