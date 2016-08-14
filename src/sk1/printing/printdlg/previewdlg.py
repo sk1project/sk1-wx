b8d9c3d0a3fc6ae4e6271ba21747263ceec41658
sK1 Project <igor.e.novikov@gmail.com>
2016-08-14 19:16:14 +0300

    PreviewDialog initial implementation

---
+++ src/sk1/printing/printdlg/previewdlg.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# 	Copyright (C) 2016 by Igor E. Novikov
+#
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import wal
+
+from sk1 import _, config
+
+class PreviewDialog(wal.SimpleDialog):
+
+	printer = None
+	printout = None
+	win = None
+	app = None
+
+	def __init__(self, win, app, printer, printout):
+		self.win = win
+		self.app = app
+		self.printer = printer
+		self.printout = printout
+		size = config.print_preview_dlg_size
+		wal.SimpleDialog.__init__(self, win, _("Print preview"),
+									size, resizable=True, add_line=False)
+		self.set_minsize(config.print_preview_dlg_minsize)
+
+	def build(self):pass
+
+	def end_modal(self, ret):
+		config.print_preview_dlg_size = self.get_size()
+		wal.SimpleDialog.end_modal(self, ret)
