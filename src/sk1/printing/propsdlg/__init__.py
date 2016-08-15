dd21aac11550dc928f5933c8021007eb5b959b43
sK1 Project <igor.e.novikov@gmail.com>
2016-08-15 11:46:57 +0300

    printer properties dialog refactored as a special module

--- src/sk1/printing/propsdlg/__init__.py
+++ src/sk1/printing/propsdlg/__init__.py
@@ -14,3 +14,5 @@
 #
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from cupspropsdlg import CUPS_PrnPropsDialog, PDF_PrnPropsDialog
