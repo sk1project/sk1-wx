b3f4b67b5f8a6c0af2b07e7a899901112b742e0f
sK1 Project <igor.e.novikov@gmail.com>
2016-08-25 19:16:02 +0300

    PDF printer properties dialog exposed

--- src/sk1/printing/propsdlg/__init__.py
+++ src/sk1/printing/propsdlg/__init__.py
@@ -15,4 +15,5 @@
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from cupspropsdlg import CUPS_PrnPropsDialog, PDF_PrnPropsDialog
+from cupspropsdlg import CUPS_PrnPropsDialog
+from pdfpropsdlg import PDF_PrnPropsDialog
