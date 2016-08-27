206c2d7708727bc79ef7e95092a72783a32da399
sK1 Project <igor.e.novikov@gmail.com>
2016-08-27 19:23:52 +0300

    final implementation

--- src/sk1/printing/propsdlg/pdfpropsdlg.py
+++ src/sk1/printing/propsdlg/pdfpropsdlg.py
@@ -67,7 +67,10 @@ class GenModePanel(wal.LabeledPanel):
 		self.compressed.set_value(self.printer.compressed)
 
 	def save(self):
-		pass
+		self.printer.compressed = self.compressed.get_value()
+		index = self.ver_combo.get_active()
+		self.printer.pdf_version = pdfconst.PDF_VERSIONS[index]
+		self.printer.colorspace = CS[self.cs_combo.get_active()]
 
 
 class PagePanel(wal.LabeledPanel):
@@ -81,7 +84,7 @@ class PagePanel(wal.LabeledPanel):
 		self.printer = printer
 		wal.LabeledPanel.__init__(self, parent, _('Document page'))
 
-		grid = wal.GridPanel(self, 2, 2, 5, 5)
+		grid = wal.GridPanel(self, 3, 2, 5, 5)
 		grid.add_growable_col(1)
 
 		grid.pack(wal.Label(grid, _('Page size:')))
@@ -95,7 +98,6 @@ class PagePanel(wal.LabeledPanel):
 
 		self.wspin = StaticUnitSpin(self.app, hpanel)
 		hpanel.pack(self.wspin)
-		hpanel.pack(StaticUnitLabel(self.app, hpanel), padding=5)
 
 		hpanel.pack((5, 5))
 
@@ -107,11 +109,28 @@ class PagePanel(wal.LabeledPanel):
 
 		grid.pack(hpanel)
 
+		grid.pack(wal.Label(grid, _('Orientation:')))
+
+		hpanel = wal.HPanel(grid)
+
+		self.port_opt = wal.Radiobutton(hpanel, _('Portrait'), group=True)
+		self.land_opt = wal.Radiobutton(hpanel, _('Landscape'))
+
+		hpanel.pack(self.port_opt)
+		hpanel.pack((15, 5))
+		hpanel.pack(self.land_opt)
+
+		grid.pack(hpanel)
+
 		self.pack(grid, fill=True, expand=True, padding_all=10)
 
 		self.set_data()
 
 	def set_data(self):
+		self.land_opt.set_value(True)
+		if self.printer.page_orientation == uc2const.PORTRAIT:
+			self.port_opt.set_value(True)
+
 		self.items = self.printer.get_format_items()
 		self.size_combo.set_items(self.items)
 		index = 0
@@ -120,6 +139,7 @@ class PagePanel(wal.LabeledPanel):
 		else:
 			index = len(self.items) - 1
 		self.size_combo.set_active(index)
+
 		if self.printer.is_custom_supported():
 			minw, minh = self.printer.customs[0]
 			maxw, maxh = self.printer.customs[1]
@@ -129,34 +149,83 @@ class PagePanel(wal.LabeledPanel):
 
 	def on_change(self):
 		index = self.size_combo.get_active()
-#		status = False
-#		if self.printer.is_custom_supported() and index == len(self.items) - 1:
-#			if not self.hspin.get_point_value() and \
-#			self.printer.def_media[:6] == 'Custom':
-#				w, h = self.printer.def_media[7:].split('x')
-#				w = float(w)
-#				h = float(h)
-#				self.wspin.set_point_value(w)
-#				self.hspin.set_point_value(h)
-#			status = True
-#		else:
-#			w, h = self.printer.pf_dict[self.printer.pf_list[index]][1]
-#			self.wspin.set_point_value(w)
-#			self.hspin.set_point_value(h)
-#		self.wspin.set_enable(status)
-#		self.hspin.set_enable(status)
+		status = False
+		if index == len(self.items) - 1:
+			if not self.hspin.get_point_value() and \
+			self.printer.page_format[0] == 'Custom':
+				w, h = self.printer.page_format[1]
+				self.wspin.set_point_value(w)
+				self.hspin.set_point_value(h)
+			status = True
+		else:
+			w, h = uc2const.PAGE_FORMATS[uc2const.PAGE_FORMAT_NAMES[index]]
+			self.wspin.set_point_value(w)
+			self.hspin.set_point_value(h)
+		self.wspin.set_enable(status)
+		self.hspin.set_enable(status)
 
 	def save(self):
 		index = self.size_combo.get_active()
-#		prn = self.printer
-#		if prn.is_custom_supported() and index == len(self.items) - 1:
-#			w = self.wspin.get_point_value()
-#			h = self.hspin.get_point_value()
-#			prn.def_media = 'Custom.%gx%g' % (w, h)
-#			prn.page_format = ('Custom', (w, h))
-#		else:
-#			prn.def_media = prn.pf_list[index]
-#			prn.page_format = prn.pf_dict[prn.def_media]
+		prn = self.printer
+		if prn.is_custom_supported() and index == len(self.items) - 1:
+			w = self.wspin.get_point_value()
+			h = self.hspin.get_point_value()
+			prn.page_format = ('Custom', (w, h))
+		else:
+			media = uc2const.PAGE_FORMAT_NAMES[index]
+			w, h = uc2const.PAGE_FORMATS[media]
+			prn.page_format = (media, (w, h))
+		self.printer.page_orientation = uc2const.LANDSCAPE
+		if self.port_opt.get_value():
+			self.printer.page_orientation = uc2const.PORTRAIT
+
+class DocInfoPanel(wal.LabeledPanel):
+
+	printer = None
+
+	def __init__(self, parent, printer, app):
+		self.printer = printer
+		self.app = app
+		wal.LabeledPanel.__init__(self, parent, _('Document metainfo'))
+
+		grid = wal.GridPanel(self, 3, 2, 2, 5)
+		grid.add_growable_col(1)
+
+		grid.pack(wal.Label(grid, _('Title:')))
+		self.title = wal.Entry(grid, self.printer.meta_title)
+		grid.pack(self.title, fill=True)
+
+		grid.pack(wal.Label(grid, _('Subject:')))
+		self.subject = wal.Entry(grid, self.printer.meta_subject)
+		grid.pack(self.subject, fill=True)
+
+		grid.pack(wal.Label(grid, _('Author:')))
+		self.author = wal.Entry(grid, self.printer.meta_author)
+		grid.pack(self.author, fill=True)
+
+		grid.pack(wal.Label(grid, _('Keywords:')))
+		self.keywords = wal.Entry(grid, self.printer.meta_keywords)
+		grid.pack(self.keywords, fill=True)
+
+		self.pack(grid, fill=True, expand=True, padding_all=10)
+
+	def is_metadata(self):
+		metainfo = self.app.current_doc.model.metainfo
+		if metainfo:
+			return bool(metainfo[0] or metainfo[2])
+		return False
+
+	def import_data(self):
+		metainfo = self.app.current_doc.model.metainfo
+		self.title.set_value('' + self.app.current_doc.doc_name)
+		self.author.set_value('' + metainfo[0])
+		self.keywords.set_value('' + metainfo[2])
+
+	def save(self):
+		self.printer.meta_title = self.title.get_value()
+		self.printer.meta_subject = self.subject.get_value()
+		self.printer.meta_author = self.author.get_value()
+		self.printer.meta_keywords = self.keywords.get_value()
 
 
 class MainPanel(wal.VPanel):
@@ -182,7 +251,10 @@ class MainPanel(wal.VPanel):
 		self.page_panel = PagePanel(self, self.printer, self.app)
 		self.pack(self.page_panel, fill=True)
 
-		self.pack((5, 5))
+		self.doc_info = DocInfoPanel(self, self.printer, self.app)
+		self.pack(self.doc_info, fill=True, expand=True)
+
+		self.panels = [self.prnmode_panel, self.page_panel, self.doc_info]
 
 	def save(self):
 		for item in self.panels: item.save()
@@ -194,6 +266,14 @@ class PDF_PrnPropsDialog(PrnProsDialog):
 		self.panel.pack((5, 5))
 		self.main_panel = MainPanel(self.panel, self.printer, self.app)
 		self.panel.pack(self.main_panel, fill=True, expand=True)
+
+	def set_dialog_buttons(self):
+		PrnProsDialog.set_dialog_buttons(self)
+		self.import_btn = wal.Button(self.left_button_box, _('Set metadata'),
+							tooltip=_('Set metadata from current document'),
+							onclick=self.main_panel.doc_info.import_data)
+		self.left_button_box.pack(self.import_btn)
+		self.import_btn.set_enable(self.main_panel.doc_info.is_metadata())
 
 	def get_result(self):
 		self.main_panel.save()
