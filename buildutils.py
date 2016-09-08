f154e0eed856a3cdd11b73cb82312717141c6663
sK1 Project <igor.e.novikov@gmail.com>
2016-09-08 20:43:25 +0300

    copyright fix

--- buildutils.py
+++ buildutils.py
@@ -2,7 +2,7 @@
 #
 #   Setup utils module
 #
-# 	Copyright (C) 2013-2015 by Igor E. Novikov
+# 	Copyright (C) 2013-2016 by Igor E. Novikov
 #
 # 	This program is free software: you can redistribute it and/or modify
 # 	it under the terms of the GNU General Public License as published by
