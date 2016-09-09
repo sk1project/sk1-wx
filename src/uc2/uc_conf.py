fe120e2257bec8877caf0b14f0779c3c8f643a7a
sK1 Project <igor.e.novikov@gmail.com>
2016-09-09 16:07:37 +0300

    mutable config path

--- src/uc2/uc_conf.py
+++ src/uc2/uc_conf.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2011 by Igor E. Novikov
+# 	Copyright (C) 2011 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
@@ -33,18 +33,21 @@ class UCData:
 	doc_icon = None
 	version = '2.0'
 	revision = 'rc1'
-	app_config_dir = os.path.expanduser(os.path.join('~', '.config', 'uc2'))
+	app_config_dir = ''
 
-	def __init__(self, app):
+	def __init__(self, app, cfgdir='~'):
 
 		self.app = app
+		if not self.app_config_dir:
+			path = os.path.expanduser(os.path.join(cfgdir, '.config', 'uc2'))
+			self.app_config_dir = path
 
 		if not os.path.lexists(self.app_config_dir):
 			os.makedirs(self.app_config_dir)
 
 		self.app_config = os.path.join(self.app_config_dir, 'preferences.cfg')
 
-		#Check color profiles directory
+		# Check color profiles directory
 		self.app_color_profile_dir = os.path.join(self.app_config_dir, 'profiles')
 		if not os.path.lexists(self.app_color_profile_dir):
 			os.makedirs(self.app_color_profile_dir)
@@ -59,7 +62,7 @@ class UCConfig(XmlConfigParser):
 
 	#============== GENERIC SECTION ===================
 	uc_version = '2.0'
-	system_encoding = 'utf-8'# default encoding (GUI uses utf-8 only)
+	system_encoding = 'utf-8'  # default encoding (GUI uses utf-8 only)
 
 
 
