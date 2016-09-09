be91bd778ec1a672890ebc58f99152ccf6fb69f6
sK1 Project <igor.e.novikov@gmail.com>
2016-09-09 17:59:04 +0300

    improved printing interface

--- src/sk1/printing/__init__.py
+++ src/sk1/printing/__init__.py
@@ -1,41 +1,41 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2016 by Igor E. Novikov
+# 	Copyright (C) 2016 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import wal
 
 from sk1 import _
 from sk1.dialogs import ProgressDialog, error_dialog
 
-from printdlg import PrintDialog
-from printout import Printout
-
 def print_dlg(parent, presenter):
 	app = presenter.app
+	printer = None
+
 	if wal.is_msw():
-		return
+		from mswprintdlg import MSWPrintDialog
+		dlg = MSWPrintDialog(parent, app, presenter)
+		ret = dlg.show()
 	else:
-		from cups_staff import CUPS_PS
-		printsys = CUPS_PS()
+		from printdlg import PrintDialog
+		dlg = PrintDialog(parent, presenter)
+		ret = dlg.show()
 
-	printout = Printout(presenter)
-	dlg = PrintDialog(parent, printsys, printout)
-	printer = dlg.show()
-
-	if printer:
+	if ret:
+		printer, printout = ret
+		if not printer: return
 		pd = ProgressDialog(_('Printing...'), parent)
 		ret = pd.run(printer.printing, [printout, ], save_result=False)
 		pd.destroy()
