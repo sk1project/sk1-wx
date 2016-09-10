5ad5e9bec23364d3928c799027a6909d2f545954
sK1 Project <igor.e.novikov@gmail.com>
2016-09-10 23:52:37 +0300

    template for canvas preferences

---
+++ src/sk1/prefs/prefs_canvas.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2016 by Igor E. Novikov
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import wal
+
+from sk1 import _, config
+from sk1.resources import icons
+
+from generic import PrefPanel
+
+class CanvasPrefs(PrefPanel):
+
+    pid = 'Canvas'
+    name = _('Canvas')
+    title = _('Canvas marker preferences')
+    icon_id = icons.PD_PREFS_PALETTE
+
+    def __init__(self, app, dlg, fmt_config=None):
+        PrefPanel.__init__(self, app, dlg)
+
+    def build(self):
+        self.built = True
