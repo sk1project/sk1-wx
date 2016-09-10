eb4cfe7fa6e3766b514e8384d2cf48ff0294d84d
sK1 Project <igor.e.novikov@gmail.com>
2016-09-10 23:48:59 +0300

    import fix

--- src/sk1/prefs/prefs_cups.py
+++ src/sk1/prefs/prefs_cups.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2016 by Igor E. Novikov
+# 	Copyright (C) 2016 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import wal
@@ -26,7 +26,7 @@ from sk1.resources import icons, get_bmp
 from sk1.printing.cups_staff import CUPS_PS, STD_MARGINS, STD_SHIFTS
 from sk1.pwidgets import StaticUnitSpin
 from sk1.dialogs import ProgressDialog, error_dialog
-from sk1.printing import Printout
+from sk1.printing.printout import Printout
 
 from generic import PrefPanel
 
