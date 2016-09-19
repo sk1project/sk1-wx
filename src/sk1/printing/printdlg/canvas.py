4fa9f4061409c2d9fc13c5c94a10b6ab8a677c48
sK1 Project <igor.e.novikov@gmail.com>
2016-09-19 19:18:04 +0300

    refactoring

--- src/sk1/printing/printdlg/canvas.py
+++ src/sk1/printing/printdlg/canvas.py
@@ -19,74 +19,14 @@ import cairo
 
 import wal
 
-from uc2 import uc2const, libimg
-from uc2.formats.sk2.crenderer import CairoRenderer
-
 from sk1.appconst import PAGEFIT, ZOOM_IN, ZOOM_OUT
 from kbd_proc import Kbd_Processor
+from printrend import PrintRenderer
 
 CAIRO_BLACK = [0.0, 0.0, 0.0]
 CAIRO_GRAY = [0.0, 0.0, 0.0, 0.5]
 CAIRO_WHITE = [1.0, 1.0, 1.0]
 CAIRO_RED = [1.0, 0.0, 0.0]
-
-class PreviewRenderer(CairoRenderer):
-
-	colorspace = uc2const.COLOR_RGB
-
-	def __init__(self, cms):
-		CairoRenderer.__init__(self, cms)
-
-	def set_colorspace(self, cs=uc2const.COLOR_RGB):
-		if not cs == self.colorspace:
-			self.colorspace = cs
-
-	def get_color(self, color):
-		if self.colorspace == uc2const.COLOR_RGB:
-			r, g, b = self.cms.get_display_color(color)
-		elif self.colorspace == uc2const.COLOR_CMYK:
-			cmyk_color = self.cms.get_cmyk_color(color)
-			r, g, b = self.cms.get_display_color(cmyk_color)
-		else:
-			gc = self.cms.get_grayscale_color(color)
-			r, g, b = self.cms.get_display_color(gc)
-		return r, g, b, color[2]
-
-	def get_image(self, pixmap):
-		if self.colorspace == uc2const.COLOR_RGB:
-			if not pixmap.cache_cdata:
-				proofing = self.cms.proofing
-				self.cms.proofing = False
-				libimg.update_image(self.cms, pixmap)
-				self.cms.proofing = proofing
-			return pixmap.cache_cdata
-		elif self.colorspace == uc2const.COLOR_CMYK:
-			if not pixmap.cache_ps_cdata:
-				libimg.update_image(self.cms, pixmap, True)
-				pixmap.cache_ps_cdata = pixmap.cache_cdata
-				pixmap.cache_cdata = None
-			return pixmap.cache_ps_cdata
-		else:
-			if not pixmap.cache_gray_cdata:
-				libimg.update_gray_image(self.cms, pixmap)
-			return pixmap.cache_gray_cdata
-
-	def get_pattern_image(self, obj):
-		if self.colorspace == uc2const.COLOR_RGB:
-			if not obj.cache_pattern_img:
-				image_obj = self._create_pattern_image(obj)
-				obj.cache_pattern_img = image_obj.cache_cdata
-			return obj.cache_pattern_img
-		elif self.colorspace == uc2const.COLOR_CMYK:
-			if not obj.cache_ps_pattern_img:
-				image_obj = self._create_pattern_image(obj, force_proofing=True)
-				obj.cache_ps_pattern_img = image_obj.cache_cdata
-			return obj.cache_ps_pattern_img
-		else:
-			if not obj.cache_gray_pattern_img:
-				image_obj = self._create_pattern_image(obj, gray=True)
-				obj.cache_gray_pattern_img = image_obj.cache_gray_cdata
-			return obj.cache_gray_pattern_img
 
 
 class PreviewCanvas(wal.Panel, wal.SensitiveCanvas):
@@ -123,7 +63,7 @@ class PreviewCanvas(wal.Panel, wal.SensitiveCanvas):
 		wal.SensitiveCanvas.__init__(self, True)
 		self.set_bg(wal.GRAY)
 		self.pages = self.printout.get_print_pages()
-		self.renderer = PreviewRenderer(self.printout.get_cms())
+		self.renderer = PrintRenderer(self.printout.get_cms())
 		self.set_double_buffered()
 
 	def destroy(self):
