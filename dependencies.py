e6ffe4aa5ba36523c0f7c3c951ff98af2e271d4b
sK1 Project <igor.e.novikov@gmail.com>
2016-09-24 18:51:59 +0300

    dist data update

--- dependencies.py
+++ dependencies.py
@@ -20,43 +20,59 @@
 import platform
 
 MINT = 'LinuxMint'
+MINT13 = 'LinuxMint 13'
+MINT17 = 'LinuxMint 17'
+MINT18 = 'LinuxMint 18'
+
 UBUNTU = 'Ubuntu'
+UBUNTU12 = 'Ubuntu 12'
 UBUNTU14 = 'Ubuntu 14'
 UBUNTU15 = 'Ubuntu 15'
 UBUNTU16 = 'Ubuntu 16'
+
+DEBIAN = ''
+DEBIAN7 = ''
+DEBIAN8 = ''
+
 FEDORA = 'fedora'
 FEDORA21 = 'fedora 21'
 FEDORA22 = 'fedora 22'
 FEDORA23 = 'fedora 23'
+FEDORA24 = 'fedora 24'
+
+OPENSUSE = ''
+OPENSUSE13 = ''
+OPENSUSE42 = ''
 
 UC2_DEB_DEPENDENCIES = {
 UBUNTU14:'libmagickwand5, liblcms2-2 (>=2.0), python (>=2.4), python (<<3.0), python-cairo, python-reportlab, python-pil',
 UBUNTU15:'libmagickwand5, liblcms2-2 (>=2.0), python (>=2.4), python (<<3.0), python-cairo, python-reportlab, python-pil',
 UBUNTU16:'libmagickwand-6.q16, liblcms2-2 (>=2.0), python (>=2.4), python (<<3.0), python-cairo, python-reportlab, python-pil',
+
+MINT17:'libmagickwand5, liblcms2-2 (>=2.0), python (>=2.4), python (<<3.0), python-cairo, python-reportlab, python-pil',
 }
 
 SK1_DEB_DEPENDENCIES = {
 UBUNTU14:'python-wxgtk2.8, python-cups',
 UBUNTU15:'python-wxgtk2.8, python-cups',
 UBUNTU16:'python-wxgtk3.0, python-cups',
+
+MINT17:'python-wxgtk2.8, python-cups',
 }
 
 UC2_RPM_DEPENDENCIES = {
 FEDORA21:'lcms2 pycairo python-pillow python-reportlab',
 FEDORA22:'lcms2 pycairo python-pillow python-reportlab',
 FEDORA23:'lcms2 pycairo python-pillow python-reportlab',
-
-'OpenSUSE':'liblcms2-2 python-cairo python-Pillow python-reportlab',
-'Mageia 64bit':'lib64lcms2_2 python-cairo python-pillow python-reportlab',
-'Mageia 32bit':'liblcms2_2 python-cairo python-pillow python-reportlab',
+FEDORA24:'lcms2 pycairo python-pillow python-reportlab',
 }
 
 #TODO: add wxpython, python-cups
 SK1_RPM_DEPENDENCIES = {
 FEDORA21:'',
-'OpenSUSE':'',
-'Mageia 64bit':'',
-'Mageia 32bit':'',
+FEDORA22:'',
+FEDORA23:'',
+FEDORA24:'',
 }
 
 
