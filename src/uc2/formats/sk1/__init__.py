61a6c61ade4eb60e257904b15660814f746c6ecc
sK1 Project <sk1project@users.noreply.github.com>
2016-10-24 02:07:52 +0300

    fix to save SK1 models

--- src/uc2/formats/sk1/__init__.py
+++ src/uc2/formats/sk1/__init__.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2013 by Igor E. Novikov
+# 	Copyright (C) 2013 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
 
-from uc2 import _, events, msgconst
+from uc2 import _, events, msgconst, uc2const
 from uc2.formats.sk1 import model
 from uc2.formats.sk1.presenter import SK1_Presenter
 from uc2.formats.pdxf.presenter import PDXF_Presenter
@@ -36,12 +36,16 @@ def sk1_loader(appdata, filename=None, fileptr=None, translate=True, cnf={}, **k
 		doc = pdxf_doc
 	return doc
 
-def sk1_saver(pdxf_doc, filename=None, fileptr=None, translate=True, cnf={}, **kw):
+def sk1_saver(sk2_doc, filename=None, fileptr=None, translate=True, cnf={}, **kw):
 	if kw: cnf.update(kw)
-	sk1_doc = SK1_Presenter(pdxf_doc.appdata, cnf)
-	sk1_doc.traslate_from_pdxf(pdxf_doc)
-	sk1_doc.save(filename)
-	sk1_doc.close()
+	if sk2_doc.cid == uc2const.SK1: translate = False
+	if translate:
+		sk1_doc = SK1_Presenter(sk2_doc.appdata, cnf)
+		sk1_doc.traslate_from_pdxf(sk2_doc)
+		sk1_doc.save(filename)
+		sk1_doc.close()
+	else:
+		sk2_doc.save(filename)
 
 def check_sk1(path):
 	fileptr = get_fileptr(path)
