c40c4309dd7fe28208b3c2a91d3b221f78a5a192
sK1 Project <sk1project@users.noreply.github.com>
2016-10-25 20:01:26 +0300

    refactoring

--- src/uc2/formats/sk1/presenter.py
+++ src/uc2/formats/sk1/presenter.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2013 by Igor E. Novikov
+# 	Copyright (C) 2013 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
@@ -22,8 +22,8 @@ from uc2.formats.generic import TextModelPresenter
 from uc2.formats.sk1.sk1_config import SK1_Config
 from uc2.formats.sk1.methods import create_new_doc, SK1_Methods
 from uc2.formats.sk1.sk1_filters import SK1_Loader, SK1_Saver
-from uc2.formats.sk1.sk1_translators import PDXF_to_SK1_Translator
-from uc2.formats.sk1.sk1_translators import SK1_to_PDXF_Translator
+from uc2.formats.sk1.sk1_translators import SK2_to_SK1_Translator
+from uc2.formats.sk1.sk1_translators import SK1_to_SK2_Translator
 
 class SK1_Presenter(TextModelPresenter):
 
@@ -49,15 +49,13 @@ class SK1_Presenter(TextModelPresenter):
 		self.model = create_new_doc(self.config)
 		self.update()
 
-	def traslate_from_pdxf(self, pdxf_doc):
-		translator = PDXF_to_SK1_Translator()
-		model = pdxf_doc.model
-		objs = [] + model.childs[0].childs[0].childs + model.childs[1].childs
-		translator.translate(objs, self)
+	def traslate_from_sk2(self, sk2_doc):
+		translator = SK2_to_SK1_Translator()
+		translator.translate(sk2_doc, self)
 
-	def traslate_to_pdxf(self, pdxf_doc):
-		translator = SK1_to_PDXF_Translator()
-		translator.translate(self, pdxf_doc)
+	def traslate_to_sk2(self, sk2_doc):
+		translator = SK1_to_SK2_Translator()
+		translator.translate(self, sk2_doc)
 
 	def update(self, action=False):
 		TextModelPresenter.update(self, action)
--- src/uc2/formats/sk1/sk1_translators.py
+++ src/uc2/formats/sk1/sk1_translators.py
@@ -1,38 +1,40 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2013 by Igor E. Novikov
-#	
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
-#	
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#	
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	Copyright (C) 2013 by Igor E. Novikov
+# 	
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+# 	
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+# 	
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from copy import deepcopy
 
 from uc2.formats.sk1 import sk1const
+from uc2 import uc2const
 
-def get_pdxf_color(clr):
+def get_sk2_color(clr):
 	if not clr: return deepcopy(sk1const.fallback_color)
 	color_spec = clr[0]
 	if color_spec == sk1const.RGB:
-		result = [sk1const.RGB, [clr[1], clr[2], clr[3]], 1.0, '']
+		result = [uc2const.COLOR_RGB, [clr[1], clr[2], clr[3]], 1.0, '', '']
 		if len(clr) == 5:result[2] = clr[4]
 		return result
 	elif color_spec == sk1const.CMYK:
-		result = [sk1const.CMYK, [clr[1], clr[2], clr[3], clr[4]], 1.0, '']
+		result = [uc2const.COLOR_CMYK,
+				[clr[1], clr[2], clr[3], clr[4]], 1.0, '', '']
 		if len(clr) == 6:result[2] = clr[5]
 		return result
 	elif color_spec == sk1const.SPOT:
-		result = [sk1const.SPOT, [[clr[3], clr[4], clr[5]],
-					[clr[6], clr[7], clr[8], clr[9]], clr[1]], 1.0, clr[2]]
+		result = [uc2const.COLOR_SPOT, [[clr[3], clr[4], clr[5]],
+					[clr[6], clr[7], clr[8], clr[9]]], 1.0, clr[2], clr[1]]
 		if len(clr) == 11:result[2] = clr[10]
 		return result
 	else:
@@ -59,7 +61,7 @@ def get_sk1_color(clr):
 	elif color_spec == sk1const.SPOT:
 		rgb = val[0]
 		cmyk = val[1]
-		pal = val[2]
+		pal = clr[4]
 		if clr[2] == 1.0:
 			result = (sk1const.SPOT, pal, clr[3], rgb[0], rgb[1], rgb[2],
 					cmyk[0], cmyk[1], cmyk[2], cmyk[3])
@@ -70,5 +72,8 @@ def get_sk1_color(clr):
 	else:
 		return deepcopy(sk1const.fallback_sk1color)
 
-class PDXF_to_SK1_Translator:pass
-class SK1_to_PDXF_Translator:pass
+class SK1_to_SK2_Translator:
+	def translate(self, sk1_doc, sk2_doc):pass
+	
+class SK2_to_SK1_Translator:
+	def translate(self, sk2_doc, sk1_doc):pass
