f06941fd74a3d238927927833bcff106075bc986
sK1 Project <sk1project@users.noreply.github.com>
2016-11-03 16:49:50 +0200

    image transparensy mask fix

--- src/uc2/formats/pdf/pdfgen.py
+++ src/uc2/formats/pdf/pdfgen.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2016 by Igor E. Novikov
+# 	Copyright (C) 2016 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import math
 from copy import deepcopy
@@ -487,6 +487,8 @@ class PDFGenerator(object):
 	def draw_pixmap(self, obj):
 		self.canvas.saveState()
 		self.canvas.transform(*obj.trafo)
+		self.canvas.setFillColorCMYK(0, 0, 0, 1, 1)
+		self.canvas.setStrokeColorCMYK(0, 0, 0, 1, 1)
 		self.draw_pixmap_obj(obj)
 		self.canvas.restoreState()
 
