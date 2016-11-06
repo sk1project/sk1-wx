705ff2eaf67a430c1b33275516e50b4995b2c7b8
sK1 Project <sk1project@users.noreply.github.com>
2016-11-06 14:17:28 +0200

    identifier line

--- src/uc2/formats/xml_/xml_filters.py
+++ src/uc2/formats/xml_/xml_filters.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2015 by Igor E. Novikov
+# 	Copyright (C) 2015 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from uc2.formats.generic_filters import AbstractXMLLoader, AbstractSaver
 from uc2.formats.xml_.xml_model import XMLObject
@@ -53,6 +53,11 @@ class XML_Saver(AbstractSaver):
 	def do_save(self):
 		cfg = self.model.config.encoding
 		self.writeln('<?xml version="1.0" encoding="%s"?>' % cfg)
+		appdata = self.presenter.appdata
+		name = "Created with %s" % appdata.app_name
+		ver = "%s%s" % (appdata.version, appdata.revision)
+		link = "(http://%s/)" % appdata.app_domain
+		self.writeln("<!-- %s %s %s) -->" % (name, ver, link))
 		self.write_obj(self.model)
 
 	def write_obj(self, obj):
