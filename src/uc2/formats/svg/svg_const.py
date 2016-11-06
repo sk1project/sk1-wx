4cf6b51e134e73b1954d2ad43c8dbdace65a1c4a
sK1 Project <sk1project@users.noreply.github.com>
2016-11-06 02:26:39 +0200

    svg constants

---
+++ src/uc2/formats/svg/svg_const.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2016 by Igor E. Novikov
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+SVG_ATTRS = {
+"xmlns":"http://www.w3.org/2000/svg",
+"xmlns:dc":"http://purl.org/dc/elements/1.1/",
+"xmlns:cc":"http://creativecommons.org/ns#",
+"xmlns:rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+"xmlns:svg":"http://www.w3.org/2000/svg",
+"xmlns:xlink":"http://www.w3.org/1999/xlink",
+"width":"210mm",
+"height":"297mm",
+"viewBox":"0 0 744.09448819 1052.3622047",
+"id":"svg2",
+"version":"1.1",
+}
