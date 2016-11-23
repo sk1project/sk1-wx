c65833bc412b3f79c15a79edc13ea1b78bb1de24
sK1 Project <sk1project@users.noreply.github.com>
2016-11-23 03:50:41 +0200

    prevent parsing clipPath

--- src/uc2/formats/svg/svg_translators.py
+++ src/uc2/formats/svg/svg_translators.py
@@ -540,6 +540,7 @@ class SVG_to_SK2_Translator(object):
 			elif svg_obj.tag == 'style':
 				self.translate_style(svg_obj)
 			elif svg_obj.tag == 'pattern': return
+			elif svg_obj.tag == 'clipPath': return
 			elif svg_obj.childs:
 				self.translate_unknown(parent, svg_obj, trafo, style)
 		except:
