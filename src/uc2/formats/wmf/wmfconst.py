4913fce9dbd828155d67c75f1b972662d7d6e150
sK1 Project <sk1project@users.noreply.github.com>
2016-11-29 00:56:42 +0200

    wmf record constants

--- src/uc2/formats/wmf/wmfconst.py
+++ src/uc2/formats/wmf/wmfconst.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# 	Copyright (C) 2013 by Igor E. Novikov
+# 	Copyright (C) 2013-2016 by Igor E. Novikov
 #
 # 	This program is free software: you can redistribute it and/or modify
 # 	it under the terms of the GNU General Public License as published by
@@ -18,8 +18,7 @@
 WMF_SIGNATURE = '\xd7\xcd\xc6\x9a'
 METAFILETYPES = ('\x01\x00', '\x02\x00')
 METAVERSIONS = ('\x00\x01', '\x00\x03')
-META_EOF = '\x03\x00\x00\x00\x00\x00'
-EOF_REC = 0x0000
+EOF_RECORD = '\x03\x00\x00\x00\x00\x00'
 
 struct_placeable_header = ('<'
 							'4s'  # Key
@@ -42,89 +41,160 @@ struct_wmf_header = ('<'
 						'I'  # Max. record size
 						'H'  # Num. Parameters
 						)
-wmf_functions = {}
-WMF_RECORD_TYPES = {
-0x0000: 'EOF Record',
-0x001E: 'SaveDC',
-0x0035: 'RealizePalette',
-0x0037: 'SetPalEntries',
+
+
+META_EOF = 0x0000
+META_SAVEDC = 0x001E
+META_REALIZEPALETTE = 0x0035
+META_SETPALENTRIES = 0x0037
+META_CREATEPALETTE = 0x00f7
+META_SETBKMODE = 0x0102
+META_SETMAPMODE = 0x0103
+META_SETROP2 = 0x0104
+META_SETRELABS = 0x0105
+META_SETPOLYFILLMODE = 0x0106
+META_SETSTRETCHBLTMODE = 0x0107
+META_SETTEXTCHAREXTRA = 0x0108
+META_RESTOREDC = 0x0127
+META_INVERTREGION = 0x012A
+META_PAINTREGION = 0x012B
+META_SELECTCLIPREGION = 0x012C
+META_SELECTOBJECT = 0x012D
+META_SETTEXTALIGN = 0x012E
+META_RESIZEPALETTE = 0x0139
+META_DIBCREATEPATTERNBRUSH = 0x0142
+META_SETLAYOUT = 0x0149
+META_DELETEOBJECT = 0x01f0
+META_CREATEPATTERNBRUSH = 0x01F9
+META_SETBKCOLOR = 0x0201
+META_SETTEXTCOLOR = 0x0209
+META_SETTEXTJUSTIFICATION = 0x020A
+META_SETWINDOWORG = 0x020B
+META_SETWINDOWEXT = 0x020C
+META_SETVIEWPORTORG = 0x020D
+META_SETVIEWPORTEXT = 0x020E
+META_OFFSETWINDOWORG = 0x020F
+META_OFFSETVIEWPORTORG = 0x0211
+META_LINETO = 0x0213
+META_MOVETO = 0x0214
+META_OFFSETCLIPRGN = 0x0220
+META_FILLREGION = 0x0228
+META_SETMAPPERFLAGS = 0x0231
+META_SELECTPALETTE = 0x0234
+META_CREATEPENINDIRECT = 0x02FA
+META_CREATEFONTINDIRECT = 0x02FB
+META_CREATEBRUSHINDIRECT = 0x02FC
+META_POLYGON = 0x0324
+META_POLYLINE = 0x0325
+META_SCALEWINDOWEXT = 0x0410
+META_SCALEVIEWPORTEXT = 0x0412
+META_EXCLUDECLIPRECT = 0x0415
+META_INTERSECTCLIPRECT = 0x0416
+META_ELLIPSE = 0x0418
+META_FLOODFILL = 0x0419
+META_RECTANGLE = 0x041B
+META_SETPIXEL = 0x041F
+META_FRAMEREGION = 0x0429
+META_ANIMATEPALETTE = 0x0436
+META_TEXTOUT = 0x0521
+META_POLYPOLYGON = 0x0538
+META_EXTFLOODFILL = 0x0548
+META_ROUNDRECT = 0x061C
+META_PATBLT = 0x061D
+META_ESCAPE = 0x0626
+META_CREATEREGION = 0x06FF
+META_ARC = 0x0817
+META_PIE = 0x081A
+META_CHORD = 0x0830
+META_BITBLT = 0x0922
+META_DIBBITBLT = 0x0940
+META_EXTTEXTOUT = 0x0a32
+META_STRETCHBLT = 0x0B23
+META_DIBSTRETCHBLT = 0x0b41
+META_SETDIBTODEV = 0x0d33
+META_STRETCHDIB = 0x0f43
+
+WMF_RECORD_NAMES = {
+META_EOF: 'EOF Record',
+META_SAVEDC: 'SaveDC',
+META_REALIZEPALETTE: 'RealizePalette',
+META_SETPALENTRIES: 'SetPalEntries',
 0x004F: 'StartPage',
 0x0050: 'EndPage',
 0x0052: 'AbortDoc',
 0x005E: 'EndDoc',
-0x00F7: 'CreatePalette',
+META_CREATEPALETTE: 'CreatePalette',
 0x00F8: 'CreateBrush',
-0x0102: 'SetBkMode',
-0x0103: 'SetMapMode',
-0x0104: 'SetROP2',
-0x0105: 'SetRelabs',
-0x0106: 'SetPolyFillMode',
-0x0107: 'SetStretchBltMode',
-0x0108: 'SetTextCharExtra',
-0x0127: 'RestoreDC',
-0x012A: 'InvertRegion',
-0x012B: 'PaintRegion',
-0x012C: 'SelectClipRegion',
-0x012D: 'SelectObject',
-0x012E: 'SetTextAlign',
-0x012E: 'SetTextAlign',
-0x0139: 'ResizePalette',
-0x0142: 'DibCreatePatternBrush',
+META_SETBKMODE: 'SetBkMode',
+META_SETMAPMODE: 'SetMapMode',
+META_SETROP2: 'SetROP2',
+META_SETRELABS: 'SetRelabs',
+META_SETPOLYFILLMODE: 'SetPolyFillMode',
+META_SETSTRETCHBLTMODE: 'SetStretchBltMode',
+META_SETTEXTCHAREXTRA: 'SetTextCharExtra',
+META_RESTOREDC: 'RestoreDC',
+META_INVERTREGION: 'InvertRegion',
+META_PAINTREGION: 'PaintRegion',
+META_SELECTCLIPREGION: 'SelectClipRegion',
+META_SELECTOBJECT: 'SelectObject',
+META_SETTEXTALIGN: 'SetTextAlign',
+META_RESIZEPALETTE: 'ResizePalette',
+META_SETLAYOUT: 'DibCreatePatternBrush',
+META_SETLAYOUT: 'SetLayout',
 0x014C: 'ResetDc',
 0x014D: 'StartDoc',
-0x01f0: 'DeleteObject',
-0x01F0: 'DeleteObject',
-0x01F9: 'CreatePatternBrush',
-0x0201: 'SetBkColor',
-0x0209: 'SetTextColor',
-0x020A: 'SetTextJustification',
-0x020B: 'SetWindowOrg',
-0x020C: 'SetWindowExt',
-0x020D: 'SetViewportOrg',
-0x020E: 'SetViewportExt',
-0x020F: 'OffsetWindowOrg',
-0x0211: 'OffsetViewportOrg',
-0x0213: 'LineTo',
-0x0214: 'MoveTo',
-0x0220: 'OffsetClipRgn',
-0x0228: 'FillRegion',
-0x0231: 'SetMapperFlags',
-0x0234: 'SelectPalette',
-0x02FA: 'CreatePenIndirect',
-0x02FB: 'CreateFontIndirect',
-0x02FC: 'CreateBrushIndirect',
+META_DELETEOBJECT: 'DeleteObject',
+META_CREATEPATTERNBRUSH: 'CreatePatternBrush',
+META_SETBKCOLOR: 'SetBkColor',
+META_SETTEXTCOLOR: 'SetTextColor',
+META_SETTEXTJUSTIFICATION: 'SetTextJustification',
+META_SETWINDOWORG: 'SetWindowOrg',
+META_SETWINDOWEXT: 'SetWindowExt',
+META_SETVIEWPORTORG: 'SetViewportOrg',
+META_SETVIEWPORTEXT: 'SetViewportExt',
+META_OFFSETWINDOWORG: 'OffsetWindowOrg',
+META_OFFSETVIEWPORTORG: 'OffsetViewportOrg',
+META_LINETO: 'LineTo',
+META_MOVETO: 'MoveTo',
+META_OFFSETCLIPRGN: 'OffsetClipRgn',
+META_FILLREGION: 'FillRegion',
+META_SETMAPPERFLAGS: 'SetMapperFlags',
+META_SELECTPALETTE: 'SelectPalette',
+META_CREATEPENINDIRECT: 'CreatePenIndirect',
+META_CREATEFONTINDIRECT: 'CreateFontIndirect',
+META_CREATEBRUSHINDIRECT: 'CreateBrushIndirect',
 0x02FD: 'CreateBitmapIndirect',
-0x0324: 'Polygon',
-0x0325: 'Polyline',
-0x0410: 'ScaleWindowExt',
-0x0412: 'ScaleViewportExt',
-0x0415: 'ExcludeClipRect',
-0x0416: 'IntersectClipRect',
-0x0418: 'Ellipse',
-0x0419: 'FloodFill',
-0x041B: 'Rectangle',
-0x041F: 'SetPixel',
-0x0429: 'FrameRegion',
-0x0436: 'AnimatePalette',
-0x0521: 'TextOut',
-0x0538: 'PolyPolygon',
-0x0548: 'ExtFloodFill',
-0x061C: 'RoundRect',
-0x061D: 'PatBlt',
-0x0626: 'Escape',
+META_POLYGON: 'Polygon',
+META_POLYLINE: 'Polyline',
+META_SCALEWINDOWEXT: 'ScaleWindowExt',
+META_SCALEVIEWPORTEXT: 'ScaleViewportExt',
+META_EXCLUDECLIPRECT: 'ExcludeClipRect',
+META_INTERSECTCLIPRECT: 'IntersectClipRect',
+META_ELLIPSE: 'Ellipse',
+META_FLOODFILL: 'FloodFill',
+META_RECTANGLE: 'Rectangle',
+META_SETPIXEL: 'SetPixel',
+META_FRAMEREGION: 'FrameRegion',
+META_ANIMATEPALETTE: 'AnimatePalette',
+META_TEXTOUT: 'TextOut',
+META_POLYPOLYGON: 'PolyPolygon',
+META_EXTFLOODFILL: 'ExtFloodFill',
+META_ROUNDRECT: 'RoundRect',
+META_PATBLT: 'PatBlt',
+META_ESCAPE: 'Escape',
 0x062F: 'DrawText',
 0x06FE: 'CreateBitmap',
-0x06FF: 'CreateRegion',
-0x0817: 'Arc',
-0x081A: 'Pie',
-0x0830: 'Chord',
-0x0922: 'BitBlt',
-0x0940: 'DibBitblt',
-0x0A32: 'ExtTextOut',
-0x0B23: 'StretchBlt',
-0x0B41: 'DibStretchBlt',
-0x0d33: 'SetDibToDev',
-0x0F43: 'StretchDIBits',
+META_CREATEREGION: 'CreateRegion',
+META_ARC: 'Arc',
+META_PIE: 'Pie',
+META_CHORD: 'Chord',
+META_BITBLT: 'BitBlt',
+META_DIBBITBLT: 'DibBitblt',
+META_EXTTEXTOUT: 'ExtTextOut',
+META_STRETCHBLT: 'StretchBlt',
+META_DIBSTRETCHBLT: 'DibStretchBlt',
+META_SETDIBTODEV: 'SetDibToDev',
+META_STRETCHDIB: 'StretchDIBits',
 }
 
 PLACEABLE_MARKUP = [(0, 4, 'WMF Signature'), (4, 2, 'HWmf handle'),
@@ -136,8 +206,11 @@ HEADER_MARKUP = [(0, 2, 'Type'), (2, 2, 'HeaderSize'), (4, 2, 'Version'),
 GENERIC_FIELDS = [(0, 4, 'Record size'), (4, 2, 'WMF record type')]
 
 RECORD_MARKUPS = {
+# State Record Types
 0x0102: [(6, 2, 'BkMode'), ],  # SetBkMode
 0x0104: [(6, 2, 'DrawMode'), ],  # SetROP2
 0x020B: [(6, 2, 'Y'), (8, 2, 'X')],  # SetWindowOrg
 0x020C: [(6, 2, 'Y'), (8, 2, 'X')],  # SetWindowExt
+
+# Drawing Record Types
 }
