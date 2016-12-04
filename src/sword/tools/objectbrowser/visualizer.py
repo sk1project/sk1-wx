228eeb5a242ec86d217d63e9b4ccdef86be801db
sK1 Project <sk1project@users.noreply.github.com>
2016-12-04 05:34:21 +0200

    color list extended

--- src/sword/tools/objectbrowser/visualizer.py
+++ src/sword/tools/objectbrowser/visualizer.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2012 by Igor E. Novikov
+# 	Copyright (C) 2012 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gtk
 import pango
@@ -270,6 +270,13 @@ COLORS = [
 	('color12', '#FFFF80'),
 	('color13', '#BAFFFA'),
 	('color14', '#F7AEFF'),
+	('color15', '#80FF80'),
+	('color16', '#FF8080'),
+	('color17', '#B0B4FF'),
+	('color18', '#D2D2D2'),
+	('color19', '#FFFF80'),
+	('color20', '#BAFFFA'),
+	('color21', '#F7AEFF'),
 	]
 
 LABEL_FG = '#BF5C00'
