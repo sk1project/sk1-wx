80a83e5ed614657dadc6700499d3a6d700141f7e
sK1 Project <sk1project@users.noreply.github.com>
2016-12-23 03:01:53 +0200

    version increment

--- setup-uc2.py
+++ setup-uc2.py
@@ -48,7 +48,7 @@ CLEAR_BUILD = False
 # Package description
 ############################################################
 NAME = 'uniconvertor'
-VERSION = '2.0rc2'
+VERSION = '2.0rc3'
 DESCRIPTION = 'Universal vector graphics translator'
 AUTHOR = 'Igor E. Novikov'
 AUTHOR_EMAIL = 'sk1.project.org@gmail.com'
