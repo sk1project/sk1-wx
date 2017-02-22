599e4bd1a1b85fd3a932af37a32f70bff7acadaa
sK1 Project <sk1project@users.noreply.github.com>
2017-02-23 01:57:02 +0200

    ACO saver/loader/checker

--- src/uc2/formats/aco/__init__.py
+++ src/uc2/formats/aco/__init__.py
@@ -14,3 +14,55 @@
 #
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from uc2.formats.generic_filters import get_fileptr
+from uc2.formats.aco.aco_const import ACO1_VER, ACO2_VER
+from uc2.formats.aco.aco_presenter import ACO_Presenter
+from uc2.formats.skp.skp_presenter import SKP_Presenter
+from uc2.formats.sk2.sk2_presenter import SK2_Presenter
+
+def aco_loader(appdata, filename=None, fileptr=None, translate=True,
+			convert=False, cnf={}, **kw):
+	if kw: cnf.update(kw)
+	doc = ACO_Presenter(appdata, cnf)
+	doc.load(filename, fileptr)
+	if convert:
+		skp_doc = SKP_Presenter(appdata, cnf)
+		doc.convert_to_skp(skp_doc)
+		doc.close()
+		return skp_doc
+	if translate:
+		skp_doc = SKP_Presenter(appdata, cnf)
+		doc.convert_to_skp(skp_doc)
+		sk2_doc = SK2_Presenter(appdata, cnf)
+		skp_doc.translate_to_sk2(sk2_doc)
+		doc.close()
+		skp_doc.close()
+		return sk2_doc
+	return doc
+
+def aco_saver(doc, filename=None, fileptr=None, translate=True,
+			convert=False, cnf={}, **kw):
+	if kw: cnf.update(kw)
+	appdata = doc.appdata
+	if translate:
+		skp_doc = SKP_Presenter(appdata, cnf)
+		skp_doc.translate_from_sk2(doc)
+		aco_doc = ACO_Presenter(appdata, cnf)
+		aco_doc.convert_from_skp(skp_doc)
+		aco_doc.save(filename, fileptr)
+		aco_doc.close()
+		skp_doc.close()
+	elif convert:
+		aco_doc = ACO_Presenter(appdata, cnf)
+		aco_doc.convert_from_skp(doc)
+		aco_doc.save(filename, fileptr)
+		aco_doc.close()
+	else:
+		doc.save(filename, fileptr)
+
+def check_ase(path):
+	fileptr = get_fileptr(path)
+	string = fileptr.read(2)
+	fileptr.close()
+	return string in (ACO1_VER, ACO2_VER)
