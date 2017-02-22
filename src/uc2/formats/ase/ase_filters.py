a4dc3e83f091b7bc2fa00889d8cf91657604b538
sK1 Project <sk1project@users.noreply.github.com>
2017-02-23 01:26:56 +0200

    import optimization

--- src/uc2/formats/ase/ase_filters.py
+++ src/uc2/formats/ase/ase_filters.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# 	Copyright (C) 2015 by Igor E. Novikov
+# 	Copyright (C) 2016 by Igor E. Novikov
 #
 # 	This program is free software: you can redistribute it and/or modify
 # 	it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 
 from uc2.formats.generic_filters import AbstractBinaryLoader, AbstractSaver
 
