7113aefc854ccd1e50c1b518a4dd9d43269dbdce
sK1 Project <sk1project@users.noreply.github.com>
2017-02-23 14:07:01 +0200

    ACO color decoding

--- src/uc2/formats/aco/aco_const.py
+++ src/uc2/formats/aco/aco_const.py
@@ -15,6 +15,9 @@
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import struct, colorsys
+from uc2 import uc2const, cms
+
 ACO1_VER = '\x00\x01'
 ACO2_VER = '\x00\x02'
 
@@ -27,6 +30,37 @@ ACO_WIDE_CMYK = 9
 
 ACO_COLORS = (ACO_RGB, ACO_HSB, ACO_CMYK, ACO_LAB, ACO_GRAY, ACO_WIDE_CMYK)
 
-def aco_chunk2color(chunk):pass
+def aco_chunk2color(chunk):
+	color = [uc2const.COLOR_RGB, [], 1.0, '', '']
+	model = struct.unpack('>H', chunk[:2])[0]
+	vals = chunk[2:10]
+	if model == ACO_RGB:
+		clr = struct.unpack('>3H', vals[:6])
+		color[1] = map(lambda x:x / 65535.0, clr)
+	elif model == ACO_HSB:
+		clr = struct.unpack('>3H', vals[:6])
+		color[1] = colorsys.hsv_to_rgb(*map(lambda x:x / 65535.0, clr))
+	elif model == ACO_CMYK:
+		clr = struct.unpack('>4H', vals)
+		color[0] = uc2const.COLOR_CMYK
+		color[1] = map(lambda x:1.0 - x / 65535.0, clr)
+	elif model == ACO_LAB:
+		L, a, b = struct.unpack('>H 2h', vals[:6])
+		clr = (L / 10000.0,
+			(a / 100.0 + 128.0) / 255.0,
+			(b / 100.0 + 128.0) / 255.0)
+		color[1] = cms.lab_to_rgb(clr)
+	elif model == ACO_GRAY:
+		color[0] = uc2const.COLOR_GRAY
+		color[1] = [struct.unpack('>H', vals[:2])[0] / 10000.0, ]
+	elif model == ACO_WIDE_CMYK:
+		clr = struct.unpack('>4H', vals)
+		color[0] = uc2const.COLOR_CMYK
+		color[1] = map(lambda x:float(x) / 10000.0, clr)
+	else:
+		return None
+	if len(chunk) > 10:
+		color[3] = chunk[12:-2].decode('utf_16_be')
+	return color
 
 def color2aco_chunk(color, version=ACO1_VER):pass
