f7bc8976a29b053735ef4c7ae60750d1ec41dbae
sK1 Project <sk1project@users.noreply.github.com>
2017-02-27 02:41:07 +0200

    refactoring to keep name convention

--- src/uc2/formats/xml_/__init__.py
+++ src/uc2/formats/xml_/__init__.py
@@ -1,37 +1,37 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2015 by Igor E. Novikov
+# 	Copyright (C) 2015 by Igor E. Novikov
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from uc2.formats.generic_filters import get_fileptr
 
 from uc2.formats.xml_.xml_presenter import XML_Presenter
 
-def xml_loader(appdata, filename=None, fileptr=None, translate=False,
+def xml__loader(appdata, filename=None, fileptr=None, translate=False,
 			convert=False, cnf={}, **kw):
 	if kw: cnf.update(kw)
 	doc = XML_Presenter(appdata, cnf)
 	doc.load(filename, fileptr)
 	return doc
 
-def xml_saver(doc, filename=None, fileptr=None, translate=False,
+def xml__saver(doc, filename=None, fileptr=None, translate=False,
 			convert=False, cnf={}, **kw):
 	if kw: cnf.update(kw)
 	doc.save(filename, fileptr)
 
-def check_xml(path):
+def check_xml_(path):
 	fileptr = get_fileptr(path)
 	ret = False
 	i = 0
