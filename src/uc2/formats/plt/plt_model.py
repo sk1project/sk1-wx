c438f5d1b4e6c41eebb05e43f9b0953515593796
sK1 Project <sk1project@users.noreply.github.com>
2017-02-27 03:28:02 +0200

    fixes for correct import

--- src/uc2/formats/plt/plt_model.py
+++ src/uc2/formats/plt/plt_model.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2012 by Igor E. Novikov
-#	
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
-#	
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#	
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 	Copyright (C) 2012 by Igor E. Novikov
+#
+# 	This program is free software: you can redistribute it and/or modify
+# 	it under the terms of the GNU General Public License as published by
+# 	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+# 	This program is distributed in the hope that it will be useful,
+# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# 	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from uc2 import _
 from uc2.formats.generic import TextModelObject
-from uc2.formats import pdxf
+from uc2.formats.sk2 import sk2_const
 
 # Document object enumeration
 HEADER = 1
@@ -137,8 +137,8 @@ class PltJob(PltModelObject):
 	initial path point. Other commands are PD commands and put down pen and do 
 	cutting job.
 	
-	Field "cache_path" store a path representation as PDXF path. Used for model 
-	translation into or from PDXF file format. Here is a sample of cache_path
+	Field "cache_path" store a path representation as SK2 path. Used for model 
+	translation into or from SK2 file format. Here is a sample of cache_path
 	content:
 	
 	[[454,11258],[[4787,11258],[4787,8711],[454,8711],[454,11258]],0]
@@ -168,7 +168,7 @@ class PltJob(PltModelObject):
 			self.cache_path = []
 			self.cache_path.append(path[0])
 			self.cache_path.append(path[1:])
-			self.cache_path.append(pdxf.const.CURVE_OPENED)
+			self.cache_path.append(sk2_const.CURVE_OPENED)
 
 
 		elif self.cache_path and not self.string:
