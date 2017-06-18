ccf3b74533940ab78d7196fe10821b263c4ece00
Igor Novikov <sk1.project.org@gmail.com>
2017-06-19 00:18:40 +0300

    module docstrings

--- src/uc2/libgeom/bbox.py
+++ src/uc2/libgeom/bbox.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# 	Copyright (C) 2015 by Igor E. Novikov
+# 	Copyright (C) 2015-2017 by Igor E. Novikov
 #
 # 	This program is free software: you can redistribute it and/or modify
 # 	it under the terms of the GNU General Public License as published by
@@ -19,30 +19,89 @@
 #------------- Bbox operations -------------
 
 def normalize_bbox(bbox):
+	"""Normalizes bounding box: sets minimal coords for first point and maximal 
+	for second point. Returns new bounding box.
+
+	:type bbox: list
+	:param bbox: bounding box
+
+	:rtype: list
+	:return: new bounding box
+	"""
 	x0, y0, x1, y1 = bbox
 	return [min(x0, x1), min(y0, y1), max(x0, x1), max(y0, y1)]
 
 def bbox_points(bbox):
+	"""Converts bounding box to list of four rectangle corners.	
+
+	:type bbox: list
+	:param bbox: bounding box
+
+	:rtype: list
+	:return: four point list
+	"""
 	x0, y0, x1, y1 = normalize_bbox(bbox)
 	return [[x0, y0], [x0, y1], [x1, y0], [x1, y1]]
 
 def bbox_middle_points(bbox):
+	"""Calcs middle points of bounding box edges.
+
+	:type bbox: list
+	:param bbox: bounding box
+
+	:rtype: list
+	:return: four point list
+	"""
 	x0, y0, x1, y1 = normalize_bbox(bbox)
 	mx = (x1 - x0) / 2.0 + x0
 	my = (y1 - y0) / 2.0 + y0
 	return [[x0, my], [mx, y1], [x1, my], [mx, y0]]
 
 def bbox_center(bbox):
+	"""Calcs bounding box center.
+
+	:type bbox: list
+	:param bbox: bounding box
+
+	:rtype: list
+	:return: point
+	"""
 	x0, y0, x1, y1 = normalize_bbox(bbox)
 	mx = (x1 - x0) / 2.0 + x0
 	my = (y1 - y0) / 2.0 + y0
 	return [mx, my]
 
 def enlarge_bbox(bbox, dx=0.0, dy=0.0):
+	"""Symmetrically enlarge bounding box.
+
+	:type bbox: list
+	:param bbox: bounding box
+
+	:type dx: float
+	:param dx: horizontal delta
+
+	:type dy: float
+	:param dy: vertical delta
+
+	:rtype: list
+	:return: new bounding box
+	"""
 	x0, y0, x1, y1 = bbox
 	return [x0 - dx / 2.0, y0 - dy / 2.0, x1 + dx / 2.0, y1 + dy / 2.0]
 
 def bbox_trafo(bbox0, bbox1):
+	"""Calcs affine transformation for changing one bounding box to another.
+
+	:type bbox0: list
+	:param bbox0: initial bounding box
+
+	:type bbox1: list
+	:param bbox2: final bounding box
+
+	:rtype: list
+	:return: affine transformation matrix
+	"""
+	#TODO: Check algorythm
 	x0_0, y0_0, x1_0, y1_0 = normalize_bbox(bbox0)
 	x0_1, y0_1, x1_1, y1_1 = normalize_bbox(bbox1)
 	w0 = x1_0 - x0_0
@@ -56,6 +115,17 @@ def bbox_trafo(bbox0, bbox1):
 	return [m11, 0.0, 0.0, m22, dx, dy]
 
 def bbox_for_point(point, size):
+	"""Calcs square bounding box for provided center and bounding box size.
+
+	:type point: list
+	:param point: bounding box center
+
+	:type size: float
+	:param size: size of square bounding box
+
+	:rtype: list
+	:return: new bounding box
+	"""
 	x0 = point[0] - size / 2.0
 	y0 = point[1] - size / 2.0
 	x1 = point[0] + size / 2.0
@@ -63,11 +133,34 @@ def bbox_for_point(point, size):
 	return [x0, y0, x1, y1]
 
 def is_point_in_bbox(point, bbox):
+	"""Checks whether is Bezier curve point inside the bounding box or not.
+
+	:type point: list
+	:param point: testing Bezier curve point
+
+	:type bbox: list
+	:param bbox: bounding box
+
+	:rtype: boolean
+	:return: boolean check result
+	"""
 	if not len(point) == 2: point = point[2]
 	return point[0] >= bbox[0] and point[1] >= bbox[1] \
 		and point[0] <= bbox[2] and point[1] <= bbox[3]
 
 def sum_bbox(bbox1, bbox2):
+	"""Summarizes two bounding boxes. The result will be bounding box which 
+	contains both provided bboxes.
+
+	:type bbox0: list
+	:param bbox0: first bounding box
+
+	:type bbox1: list
+	:param bbox2: second bounding box
+
+	:rtype: list
+	:return: new bounding box
+	"""
 	if not bbox1 or not bbox2:
 		return bbox1 + bbox2
 	x0, y0, x1, y1 = bbox1
@@ -79,6 +172,18 @@ def sum_bbox(bbox1, bbox2):
 	return [new_x0, new_y0, new_x1, new_y1]
 
 def is_bbox_in_rect(rect, bbox):
+	"""Checks whether is second bounding box inside the first 
+	bounding box or not.
+
+	:type rect: list
+	:param rect: second bounding box
+
+	:type bbox: list
+	:param bbox: first bounding box
+
+	:rtype: boolean
+	:return: boolean check result
+	"""
 	x0, y0, x1, y1 = rect
 	_x0, _y0, _x1, _y1 = bbox
 	if x0 > _x0: return False
@@ -88,6 +193,18 @@ def is_bbox_in_rect(rect, bbox):
 	return True
 
 def is_point_in_rect(point, rect):
+	"""Checks whether is coordinate point inside the rectangle or not.
+	Rectangle is defined by bounding box.
+
+	:type point: list
+	:param point: testing coordinate point
+
+	:type rect: list
+	:param rect: bounding box
+
+	:rtype: boolean
+	:return: boolean check result
+	"""
 	x0, y0, x1, y1 = rect
 	x, y = point
 	if x0 <= x <= x1 and y0 <= y <= y1:
@@ -95,6 +212,24 @@ def is_point_in_rect(point, rect):
 	return False
 
 def is_point_in_rect2(point, rect_center, rect_w, rect_h):
+	"""Checks whether is coordinate point inside the rectangle or not.
+	Rectangle is defined by center and linear sizes.
+
+	:type point: list
+	:param point: testing coordinate point
+
+	:type rect_center\: list
+	:param rect_center: point, center of rectangle
+
+	:type rect_w: float
+	:param rect_w: rectangle width
+
+	:type rect_h: float
+	:param rect_h: rectangle height
+
+	:rtype: boolean
+	:return: boolean check result
+	"""
 	cx, cy = rect_center
 	x, y = point
 	if abs(x - cx) <= rect_w / 2.0 and abs(y - cy) <= rect_h / 2.0:
@@ -102,10 +237,29 @@ def is_point_in_rect2(point, rect_center, rect_w, rect_h):
 	return False
 
 def bbox_size(bbox):
+	"""Calcs bounding box width and height.
+
+	:type bbox: list
+	:param bbox: bounding box
+
+	:rtype: tuple
+	:return: width and height
+	"""
 	x0, y0, x1, y1 = bbox
 	return abs(x1 - x0), abs(y1 - y0)
 
 def is_bbox_overlap(bbox1, bbox2):
+	"""Checks whether are bounding boxes overlapped or not.
+
+	:type bbox1: list
+	:param bbox1: first bounding box
+
+	:type bbox2: list
+	:param bbox2: second bounding box
+
+	:rtype: boolean
+	:return: boolean check result
+	"""
 	new_bbox = sum_bbox(bbox1, bbox2)
 	w1, h1 = bbox_size(bbox1)
 	w2, h2 = bbox_size(bbox2)
@@ -113,6 +267,18 @@ def is_bbox_overlap(bbox1, bbox2):
 	return w <= w1 + w2 and h <= h1 + h2
 
 def is_bbox_in_bbox(bbox1, bbox2):
+	"""Checks whether is one bounding box inside another 
+	bounding box or not.
+
+	:type bbox1: list
+	:param bbox1: first bounding box
+
+	:type bbox2: list
+	:param bbox2: second bounding box
+
+	:rtype: boolean
+	:return: boolean check result
+	"""
 	new_bbox = sum_bbox(bbox1, bbox2)
 	w1, h1 = bbox_size(bbox1)
 	w2, h2 = bbox_size(bbox2)
@@ -122,6 +288,14 @@ def is_bbox_in_bbox(bbox1, bbox2):
 	return False
 
 def bbox_for_points(points):
+	"""Finds bounding box for provided points.
+
+	:type points: list or tuple
+	:param points: sequince of coordinate points
+
+	:rtype: list
+	:return: bounding box
+	"""
 	xmin = xmax = points[0][0]
 	ymin = ymax = points[0][1]
 	for point in points:
