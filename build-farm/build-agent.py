ac2613d45db979d0ada34423567565395472c722
Igor Novikov <i.novikov@scalr.com>
2017-07-28 16:58:52 +0300

    deb build

--- build-farm/build-agent.py
+++ build-farm/build-agent.py
@@ -33,27 +33,97 @@
 
 import sys, os, platform
 
-WINDOWS = 'Windows'
-LINUX = 'Linux'
-MACOS = 'Darwin'
 
+class Error(Exception): pass
 
-def is_msw(): return platform.system() == WINDOWS
-def is_linux(): return platform.system() == LINUX
-def is_macos(): return platform.system() == MACOS
-def is_path(path): return os.path.lexists(path)
-
-
-BUILD_DIR = os.path.expanduser('~/buildfarm')
 
 DATASET = {
     'project': 'sk1-wx',
     'project2': 'sk1-wx-msw',
     'git_url': 'https://github.com/sk1project/sk1-wx',
-    'git_url2': '',
+    'git_url2': 'https://github.com/sk1project/sk1-wx-msw',
     'ftp_url': 'ftp://builder:password@192.168.0.102',
-    'timestamp': ''
+    'timestamp': '',
+    'script': 'setup-sk1.py',
 }
+
+WINDOWS = 'Windows'
+LINUX = 'Linux'
+MACOS = 'Darwin'
+
+
+def is_msw():
+    return platform.system() == WINDOWS
+
+
+def is_linux():
+    return platform.system() == LINUX
+
+
+def is_macos():
+    return platform.system() == MACOS
+
+
+def is_path(path):
+    return os.path.lexists(path)
+
+
+MINT = 'LinuxMint'
+UBUNTU = 'Ubuntu'
+DEBIAN = 'debian'
+FEDORA = 'fedora'
+OPENSUSE = 'SuSE'
+
+MARKERS = {
+    MINT: 'mint',
+    UBUNTU: 'ubuntu',
+    DEBIAN: 'debian',
+    FEDORA: 'fc',
+    OPENSUSE: 'opensuse',
+}
+
+
+def is_deb():
+    return platform.dist()[0] in [MINT, UBUNTU, DEBIAN]
+
+
+def is_debian():
+    return platform.dist()[0] == DEBIAN
+
+
+def is_ubuntu():
+    return platform.dist()[0] == UBUNTU
+
+
+def is_rpm():
+    return platform.dist()[0] in [FEDORA, OPENSUSE]
+
+
+def get_marker():
+    if is_linux():
+        if is_deb():
+            ver = platform.dist()[1]
+            if is_debian(): ver = ver.split('.')[0]
+            marker = '_%s_%s_' % (MARKERS[platform.dist()[0]], ver)
+            if DATASET['timestamp']:
+                marker = '_%s%s' % (DATASET['timestamp'], marker)
+            return marker
+        return MARKERS[platform.dist()[0]]
+    elif is_msw():
+        return 'win'
+    return 'macos'
+
+
+def listdir(path):
+    result = []
+    items = os.listdir(path)
+    for item in items:
+        if os.path.isfile(item):
+            result.append(item)
+    return result
+
+
+# ------------ Build script ------------------
 
 # CLI args processing
 if len(sys.argv) > 1:
@@ -65,7 +135,40 @@ if len(sys.argv) > 1:
             if value[-1] in ('"', "'"): value = value[:-1]
             DATASET[key] = value
 
+BUILD_DIR = os.path.expanduser('~/buildfarm')
 PROJECT_DIR = os.path.join(BUILD_DIR, DATASET['project'])
 PROJECT2_DIR = os.path.join(BUILD_DIR, DATASET['project2'])
+DIST_DIR = os.path.join(PROJECT_DIR, 'dist')
 
-if not os.path.lexists(BUILD_DIR): os.mkdir(BUILD_DIR)
+try:
+    if not is_path(BUILD_DIR): os.mkdir(BUILD_DIR)
+
+    if is_linux():
+        if is_deb():
+            if not is_path(PROJECT_DIR):
+                os.system('cd %s;git clone %s %s' % (BUILD_DIR, DATASET['git_url'], DATASET['project']))
+            else:
+                os.system('cd %s;git pull' % PROJECT_DIR)
+            if is_path(DIST_DIR):
+                os.system('rm -rf %s' % DIST_DIR)
+
+            os.system('cd %s;python %s bdist_deb' % (PROJECT_DIR, DATASET['script']))
+
+            files = listdir(DIST_DIR)
+            if len(files) == 1:
+                package_name = files[0]
+            else:
+                raise Error('Build failed!')
+            new_name = '%s%s%s' % (package_name.split('_')[0], get_marker(), package_name.split('_')[1])
+            new_name = os.path.join(DIST_DIR, new_name)
+            old_name = os.path.join(DIST_DIR, package_name)
+            os.system('mv %s %s' % (old_name, new_name))
+
+        elif is_rpm():
+            pass
+    elif is_msw():
+        pass
+    elif is_macos():
+        pass
+except:
+    sys.exit(1)
