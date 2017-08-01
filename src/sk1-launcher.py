fc4bb392205bfe55f92b42b9147078ceffac90d3
Igor E. Novikov <sk1.project.org@gmail.com>
2017-08-01 12:03:12 +0300

    reformatting for PEP8

--- src/sk1-launcher.py
+++ src/sk1-launcher.py
@@ -17,41 +17,42 @@
 # 	You should have received a copy of the GNU General Public License
 # 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, platform
+import os
+import platform
 
 RESTRICTED = ('UniConvertor', 'Python', 'ImageMagick')
 
 
 def get_path_var():
-	path = '' + os.environ["PATH"]
-	paths = path.split(os.pathsep)
-	ret = []
-	for path in paths:
-		allow = True
-		for item in RESTRICTED:
-			if item in path: allow = False
-		if allow: ret.append(path)
-	return os.pathsep.join(ret)
+    path = '' + os.environ["PATH"]
+    paths = path.split(os.pathsep)
+    ret = []
+    for path in paths:
+        for item in RESTRICTED:
+            if item not in path:
+                ret.append(path)
+    return os.pathsep.join(ret)
 
 
 if os.name == 'nt':
 
-	cur_path = os.path.abspath('..\\..\\sk1-wx-msw')
+    cur_path = os.path.abspath('..\\..\\sk1-wx-msw')
 
-	devresdir = 'win32-devres'
-	if platform.architecture()[0] == '64bit': devresdir = 'win64-devres'
+    devresdir = 'win32-devres'
+    if platform.architecture()[0] == '64bit':
+        devresdir = 'win64-devres'
 
-	devres = os.path.join(cur_path, devresdir)
-	bindir = os.path.join(devres, 'dlls') + os.pathsep
-	magickdir = os.path.join(devres, 'dlls', 'modules') + os.pathsep
+    devres = os.path.join(cur_path, devresdir)
+    bindir = os.path.join(devres, 'dlls') + os.pathsep
+    magickdir = os.path.join(devres, 'dlls', 'modules') + os.pathsep
 
-	os.environ["PATH"] = magickdir + bindir + get_path_var()
-	os.environ["MAGICK_CODER_MODULE_PATH"] = magickdir
-	os.environ["MAGICK_CODER_FILTER_PATH"] = magickdir
-	os.environ["MAGICK_CONFIGURE_PATH"] = magickdir
-	os.environ["MAGICK_HOME"] = magickdir
+    os.environ["PATH"] = magickdir + bindir + get_path_var()
+    os.environ["MAGICK_CODER_MODULE_PATH"] = magickdir
+    os.environ["MAGICK_CODER_FILTER_PATH"] = magickdir
+    os.environ["MAGICK_CONFIGURE_PATH"] = magickdir
+    os.environ["MAGICK_HOME"] = magickdir
 
-	os.chdir(os.path.join(devres, 'dlls'))
+    os.chdir(os.path.join(devres, 'dlls'))
 
 import sk1
 
