26e6072624ee286815693836e3edd1b9e076aed3
Igor E. Novikov <sk1.project.org@gmail.com>
2017-08-23 11:49:22 +0300

    build farm wip

--- build-farm/farm-sk1.py
+++ build-farm/farm-sk1.py
@@ -29,6 +29,18 @@
 #           2)Configure your system for building kernel modules by running:
 #               m-a prepare
 #           3)Run sh VBoxLinuxAdditions.run
+#        for OpenSuse:
+#           1)Update system:
+#               sudo zypper refresh;sudo zypper update
+#           2)Install tools for kernel module build:
+#               sudo zypper in kernel-syms gcc make rpm-build
+#           3)Insert addons disk and do autorun
+#        for Fedora
+#           1)Update system:
+#               dnf update
+#           2)Install tools for kernel module build:
+#               dnf -y install gcc automake make kernel-headers kernel-devel
+#               dnf -y install dkms bzip2 perl rpm-build
 #   3.Copy build-agent.py to home dir
 #   4.To execute sudo you need adding in /etc/sudoers following line:
 #       username ALL = NOPASSWD: ALL
@@ -37,8 +49,17 @@ import datetime
 import os
 import sys
 
+STDOUT_MAGENTA = '\033[95m'
+STDOUT_BLUE = '\033[94m'
+STDOUT_GREEN = '\033[92m'
+STDOUT_YELLOW = '\033[93m'
+STDOUT_FAIL = '\033[91m'
+STDOUT_ENDC = '\033[0m'
+STDOUT_BOLD = '\033[1m'
+STDOUT_UNDERLINE = '\033[4m'
+
 DATASET = {
-    'agent_ver': '1.0.2',
+    'agent_ver': '1.0.6',
     'mode': 'publish',
     # publish - to build and publish build result
     # release - to prepare release build
@@ -59,25 +80,27 @@ DATASET = {
 }
 
 DEB = [
-    # 'Ubuntu 14.04 32bit',
-    # 'Ubuntu 14.04 64bit',
-    # 'Ubuntu 16.04 32bit',
-    # 'Ubuntu 16.04 64bit',
-    # 'Ubuntu 17.04 32bit',
-    # 'Ubuntu 17.04 64bit',
-    # 'Debian 7.8 32bit',
-    # 'Debian 7.8 64bit',
+    'Ubuntu 14.04 32bit',
+    'Ubuntu 14.04 64bit',
+    'Ubuntu 16.04 32bit',
+    'Ubuntu 16.04 64bit',
+    'Ubuntu 17.04 32bit',
+    'Ubuntu 17.04 64bit',
+    'Debian 7.8 32bit',
+    'Debian 7.8 64bit',
     'Debian 8.6 32bit',
-    # 'Debian 8.6 64bit',
+    'Debian 8.6 64bit',
+    'Debian 9.1 32bit',
+    'Debian 9.1 64bit',
 ]
 RPM = [
-    # 'Fedora 23 32bit',
-    # 'Fedora 23 64bit',
-    # 'Fedora 24 32bit',
-    # 'Fedora 24 64bit',
-    # 'OpenSuse 13.2 32bit',
-    # 'OpenSuse 13.2 64bit',
-    # 'OpenSuse 42.1 64bit',
+    'Fedora 25 32bit',
+    'Fedora 25 64bit',
+    # 'Fedora 26 32bit',
+    # 'Fedora 26 64bit',
+    'OpenSuse 42.1 64bit',
+    'OpenSuse 42.2 64bit',
+    'OpenSuse 42.3 64bit',
 ]
 MSI = [
     # 'Win7 32bit',
@@ -89,9 +112,11 @@ OSes = DEB + RPM + MSI
 VMTYPE = 'headless'  # 'gui'
 
 
-def echo_msg(msg, newline=True, flush=True):
+def echo_msg(msg, newline=True, flush=True, code=''):
     if newline:
         msg += '\n'
+    if code:
+        msg = code + msg + STDOUT_ENDC
     sys.stdout.write(msg)
     if flush:
         sys.stdout.flush()
@@ -107,7 +132,7 @@ def suspendvm(vmname):
 
 
 def run_agent(vmname):
-    echo_msg('\n===>STARTING BUILD ON "%s"' % vmname)
+    echo_msg('\n===>STARTING BUILD ON "%s"' % vmname, code=STDOUT_GREEN)
     cmd = 'VBoxManage --nologo guestcontrol "%s" run' % vmname
     cmd += ' --exe "/usr/bin/sudo"'
     cmd += ' --username "%s"' % DATASET['user']
@@ -124,14 +149,17 @@ def run_agent(vmname):
         cmd += ' "%s=%s"' % (item, value)
 
     os.system(cmd)
-    echo_msg('===>BUILD FINISHED ON "%s"' % vmname)
+    echo_msg('===>BUILD FINISHED ON "%s"' % vmname, code=STDOUT_GREEN)
 
 
-echo_msg('\n' + '|' * 20 + 'FARM STARTED' + '|' * 20 + '\n')
+flr_left = '\n' + '|' * 20
+flr_right = '|' * 20 + '\n'
+echo_msg(flr_left + ' FARM STARTED ' + flr_right, code=STDOUT_YELLOW)
 
 for vmname in OSes:
+    echo_msg(vmname + '-' * 40 + '\n', code=STDOUT_BOLD)
     startvm(vmname)
     run_agent(vmname)
     suspendvm(vmname)
 
-echo_msg('\n' + '|' * 20 + 'FARM TERMINATED' + '|' * 20 + '\n')
+echo_msg(flr_left + 'FARM TERMINATED' + flr_right, code=STDOUT_YELLOW)
