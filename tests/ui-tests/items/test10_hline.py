bbecd6ca71ee6ae9434c9de55e291b87e729f8ee
Igor Novikov <sk1.project.org@gmail.com>
2017-08-27 00:32:09 +0300

    VLine, HLine tests

--- tests/ui-tests/items/test10_hline.py
+++ tests/ui-tests/items/test10_hline.py
@@ -1,2 +1,17 @@
 #The test should show horizontal 
-#line (Hline) on the main window
+#line (HLine) on the main window
+
+import wal
+
+SIZE = (300, 200)
+
+class MW(wal.MainWindow):
+
+    def __init__(self):
+        wal.MainWindow.__init__(self, vertical=False)
+        self.set_size(SIZE)
+
+        self.pack(wal.HLine(self), expand=True, fill=True)
+
+
+MW().run()
