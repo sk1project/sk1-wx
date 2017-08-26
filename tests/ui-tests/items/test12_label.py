58e0991c1d75c1dd4330c0f77626e7950a2e5acb
Igor Novikov <sk1.project.org@gmail.com>
2017-08-27 00:52:34 +0300

    Label test

---
+++ tests/ui-tests/items/test12_label.py
@@ -0,0 +1,24 @@
+#The test should show vertical 
+#line (VLine) on the main window
+
+import wal
+
+SIZE = (300, 300)
+
+class MW(wal.MainWindow):
+
+    def __init__(self):
+        wal.MainWindow.__init__(self)
+        self.set_size(SIZE)
+
+        self.pack(wal.Label(self,'Regular label'), padding=10)
+        self.pack(wal.Label(self,'Bold label', fontbold=True), padding=10)
+        self.pack(wal.Label(self,'Large label', fontsize=2), padding=10)
+        self.pack(wal.Label(self,'Small label', fontsize=-1), padding=10)
+        self.pack(wal.Label(self,'Colored label', fg=wal.RED), padding=10)
+        label = wal.Label(self,'Disabled label')
+        label.set_enable(False)
+        self.pack(label, padding=10)
+
+
+MW().run()
