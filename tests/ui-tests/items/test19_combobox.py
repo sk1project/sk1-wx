7e09a71c37bdf0c41c8731f77f1bae73f4ea73d6
Igor Novikov <sk1.project.org@gmail.com>
2017-08-28 12:03:46 +0300

    combobox test

---
+++ tests/ui-tests/items/test19_combobox.py
@@ -0,0 +1,33 @@
+#The test should show Combobox and
+#FloatCombobox widgets on the main window
+
+import wal
+
+SIZE = (300, 200)
+
+class MW(wal.MainWindow):
+
+    def __init__(self):
+        wal.MainWindow.__init__(self)
+        self.set_size(SIZE)
+
+        items = ['One', 'Two', 'Three']
+
+        self.pack(wal.Label(self, 'Combobox:'))
+        self.cmb = wal.Combobox(self, items=items, onchange=self.on_change)
+        self.pack(self.cmb)
+
+        self.pack((10,10))
+
+        self.pack(wal.Label(self, 'FloatCombobox:'))
+        self.fcmb = wal.FloatCombobox(self, items=range(10), 
+            onchange=self.on_change2)
+        self.pack(self.fcmb)
+
+    def on_change(self):
+        print self.cmb.get_value()
+
+    def on_change2(self):
+        print self.fcmb.get_value()
+
+MW().run()
