e5d80504e2f0f8244fb9a97bdf4db209b5dc9bc2
Igor E. Novikov <sk1.project.org@gmail.com>
2017-08-29 02:25:50 +0300

    refactoring

--- tests/ui-tests/items/test21_spin.py
+++ tests/ui-tests/items/test21_spin.py
@@ -1,20 +1,31 @@
-#The test should show Spin widget 
-#on the main window
+# The test should show Spin widget
+# on the main window
 
 import wal
 
 SIZE = (300, 200)
 
-class MW(wal.MainWindow):
 
+class MW(wal.MainWindow):
     def __init__(self):
         wal.MainWindow.__init__(self)
         self.set_size(SIZE)
 
-        self.spin = wal.Spin(self, 5, (0,20), onchange=self.on_change)
+        self.spin = wal.Spin(self, 5, (0, 20), width=3, onchange=self.onchange,
+            onenter=self.onenter)
         self.pack(self.spin)
 
-    def on_change(self):
+        self.spin1 = wal.Spin(self, 5, (0, 20), onchange=self.onchange1)
+        self.pack(self.spin1)
+
+    def onchange(self):
         print self.spin.get_value()
-        
+
+    def onenter(self):
+        print 'onenter', self.spin.get_value()
+
+    def onchange1(self):
+        print self.spin1.get_value()
+
+
 MW().run()
