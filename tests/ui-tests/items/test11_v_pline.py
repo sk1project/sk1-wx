1560d188517c31993891383ad075fb8da1a6ade1
Igor Novikov <sk1.project.org@gmail.com>
2017-09-25 07:59:32 +0300

    PLine tests

---
+++ tests/ui-tests/items/test11_v_pline.py
@@ -0,0 +1,18 @@
+#The test should show vertical 
+#line (PLine) on the main window
+
+import wal
+
+SIZE = (300, 200)
+
+class MW(wal.MainWindow):
+
+    def __init__(self):
+        wal.MainWindow.__init__(self, vertical=False)
+        self.set_size(SIZE)
+
+        self.pack((10,10))
+        self.pack(wal.PLine(self), fill=True)
+
+
+MW().run()
