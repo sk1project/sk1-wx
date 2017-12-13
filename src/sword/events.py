ef39188c2d049ace0d7a3ec798c1297b123a433f
Igor E. Novikov <sk1.project.org@gmail.com>
2017-12-13 10:54:44 +0200

    sword package refactoring

--- src/sword/events.py
+++ src/sword/events.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-#	Copyright (C) 2011 by Igor E. Novikov
-#	
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
-#	
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#	
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#  Copyright (C) 2011 by Igor E. Novikov
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 The package provides Qt-like signal-slot functionality
@@ -33,7 +33,7 @@ BIN_SELECTION arg[0] - selection
 CLIPBOARD		 no args 
 """
 
-#Signal channels
+# Signal channels
 
 CONFIG_MODIFIED = ['CONFIG_MODIFIED']
 
@@ -51,47 +51,43 @@ BIN_SELECTION = ['BIN_SELECTION']
 CLIPBOARD = ['CLIPBOARD']
 
 
-
 def connect(channel, receiver):
-	"""
-	Connects signal receive method
-	to provided channel. 
-	"""
-	if callable(receiver):
-		try:
-			channel.append(receiver)
-		except:
-			msg = "Cannot connect to channel:"
-			print msg, channel, "receiver:", receiver
+    """
+    Connects signal receive method
+    to provided channel.
+    """
+    if callable(receiver):
+        try:
+            channel.append(receiver)
+        except:
+            msg = "Cannot connect to channel:"
+            print msg, channel, "receiver:", receiver
+
 
 def disconnect(channel, receiver):
-	"""
-	Disconnects signal receive method
-	from provided channel. 
-	"""
-	if callable(receiver):
-		try:
-			channel.remove(receiver)
-		except:
-			msg = "Cannot disconnect from channel:"
-			print msg, channel, "receiver:", receiver
+    """
+    Disconnects signal receive method
+    from provided channel.
+    """
+    if callable(receiver):
+        try:
+            channel.remove(receiver)
+        except:
+            msg = "Cannot disconnect from channel:"
+            print msg, channel, "receiver:", receiver
+
 
 def emit(channel, *args):
-	"""
-	Sends signal to all receivers in channel.
-	"""
-#	print 'signal', channel[0]
-	try:
-		for receiver in channel[1:]:
-			try:
-				if callable(receiver):
-					receiver(args)
-			except:
-				pass
-	except:
-		print "Cannot send signal to channel:", channel
-
-
-
-
-
+    """
+    Sends signal to all receivers in channel.
+    """
+    #	print 'signal', channel[0]
+    try:
+        for receiver in channel[1:]:
+            try:
+                if callable(receiver):
+                    receiver(args)
+            except:
+                pass
+    except:
+        print "Cannot send signal to channel:", channel
