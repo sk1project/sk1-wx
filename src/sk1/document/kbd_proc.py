4f004022220745faa047515c2ec683c01969dcfb
Igor E. Novikov <sk1.project.org@gmail.com>
2018-01-04 22:00:56 +0200

    kbd_proc wal vs wx refactoring

--- src/sk1/document/kbd_proc.py
+++ src/sk1/document/kbd_proc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-#  Copyright (C) 2015-2016 by Igor E. Novikov
+#  Copyright (C) 2015-2018 by Igor E. Novikov
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import wx
+import wal
 
 from sk1 import modes
 from sk1.resources import pdids
@@ -34,43 +34,43 @@ class KbdProcessor:
         raw_code = event.GetRawKeyCode()
         modifiers = event.GetModifiers()
 
-        if key_code == wx.WXK_ESCAPE and not modifiers:
+        if key_code == wal.KEY_ESCAPE and not modifiers:
             self.canvas.controller.escape_pressed()
             return
 
-        if key_code == wx.WXK_NUMPAD_DECIMAL and modifiers == wx.ACCEL_SHIFT:
-            self.actions[wx.ID_CUT].do_call()
+        if key_code == wal.KEY_NUMPAD_DECIMAL and modifiers == wal.ACCEL_SHIFT:
+            self.actions[wal.ID_CUT].do_call()
             return
 
-        if key_code == wx.WXK_NUMPAD0 and modifiers == wx.ACCEL_SHIFT:
-            self.actions[wx.ID_PASTE].do_call()
+        if key_code == wal.KEY_NUMPAD0 and modifiers == wal.ACCEL_SHIFT:
+            self.actions[wal.ID_PASTE].do_call()
             return
 
         if self.canvas.mode == modes.TEXT_EDIT_MODE:
             self.text_edit_mode(event, key_code, raw_code, modifiers)
             return
 
-        if key_code in (wx.WXK_UP, wx.WXK_NUMPAD_UP) and not modifiers:
+        if key_code in (wal.KEY_UP, wal.KEY_NUMPAD_UP) and not modifiers:
             self.actions[pdids.MOVE_UP].do_call()
             return
 
-        if key_code in (wx.WXK_DOWN, wx.WXK_NUMPAD_DOWN) and not modifiers:
+        if key_code in (wal.KEY_DOWN, wal.KEY_NUMPAD_DOWN) and not modifiers:
             self.actions[pdids.MOVE_DOWN].do_call()
             return
 
-        if key_code in (wx.WXK_LEFT, wx.WXK_NUMPAD_LEFT) and not modifiers:
+        if key_code in (wal.KEY_LEFT, wal.KEY_NUMPAD_LEFT) and not modifiers:
             self.actions[pdids.MOVE_LEFT].do_call()
             return
 
-        if key_code in (wx.WXK_RIGHT, wx.WXK_NUMPAD_RIGHT) and not modifiers:
+        if key_code in (wal.KEY_RIGHT, wal.KEY_NUMPAD_RIGHT) and not modifiers:
             self.actions[pdids.MOVE_RIGHT].do_call()
             return
 
-        if key_code == wx.WXK_F2 and not modifiers:
+        if key_code == wal.KEY_F2 and not modifiers:
             self.canvas.set_mode(modes.ZOOM_MODE)
             return
 
-        if key_code == wx.WXK_SPACE and not modifiers:
+        if key_code == wal.KEY_SPACE and not modifiers:
             if self.canvas.mode == modes.SELECT_MODE:
                 self.canvas.set_mode(modes.SHAPER_MODE)
                 return
@@ -82,64 +82,64 @@ class KbdProcessor:
 
     def text_edit_mode(self, event, key_code, raw_code, modifiers):
         if not modifiers:
-            if key_code in (wx.WXK_UP, wx.WXK_NUMPAD_UP):
+            if key_code in (wal.KEY_UP, wal.KEY_NUMPAD_UP):
                 self.canvas.controller.key_up()
                 return
-            if key_code in (wx.WXK_DOWN, wx.WXK_NUMPAD_DOWN):
+            if key_code in (wal.KEY_DOWN, wal.KEY_NUMPAD_DOWN):
                 self.canvas.controller.key_down()
                 return
-            if key_code in (wx.WXK_LEFT, wx.WXK_NUMPAD_LEFT):
+            if key_code in (wal.KEY_LEFT, wal.KEY_NUMPAD_LEFT):
                 self.canvas.controller.key_left()
                 return
-            if key_code in (wx.WXK_RIGHT, wx.WXK_NUMPAD_RIGHT):
+            if key_code in (wal.KEY_RIGHT, wal.KEY_NUMPAD_RIGHT):
                 self.canvas.controller.key_right()
                 return
-            if key_code in (wx.WXK_HOME, wx.WXK_NUMPAD_HOME):
+            if key_code in (wal.KEY_HOME, wal.KEY_NUMPAD_HOME):
                 self.canvas.controller.key_home()
                 return
-            if key_code in (wx.WXK_END, wx.WXK_NUMPAD_END):
+            if key_code in (wal.KEY_END, wal.KEY_NUMPAD_END):
                 self.canvas.controller.key_end()
                 return
-            if key_code == wx.WXK_BACK:
+            if key_code == wal.KEY_BACK:
                 self.canvas.controller.key_backspace()
                 return
-            if key_code in (wx.WXK_RETURN, wx.WXK_NUMPAD_ENTER):
+            if key_code in (wal.KEY_RETURN, wal.KEY_NUMPAD_ENTER):
                 self.canvas.controller.insert_text('\n')
                 return
-            if key_code in (wx.WXK_DELETE, wx.WXK_NUMPAD_DELETE):
+            if key_code in (wal.KEY_DELETE, wal.KEY_NUMPAD_DELETE):
                 self.canvas.controller.key_del()
                 return
-        elif modifiers == wx.ACCEL_CTRL:
-            if key_code in (wx.WXK_HOME, wx.WXK_NUMPAD_HOME):
+        elif modifiers == wal.ACCEL_CTRL:
+            if key_code in (wal.KEY_HOME, wal.KEY_NUMPAD_HOME):
                 self.canvas.controller.key_ctrl_home()
                 return
-            if key_code in (wx.WXK_END, wx.WXK_NUMPAD_END):
+            if key_code in (wal.KEY_END, wal.KEY_NUMPAD_END):
                 self.canvas.controller.key_ctrl_end()
                 return
-        elif modifiers == wx.ACCEL_CTRL | wx.ACCEL_SHIFT:
-            if key_code in (wx.WXK_HOME, wx.WXK_NUMPAD_HOME):
+        elif modifiers == wal.ACCEL_CTRL | wal.ACCEL_SHIFT:
+            if key_code in (wal.KEY_HOME, wal.KEY_NUMPAD_HOME):
                 self.canvas.controller.key_ctrl_home(True)
                 return
-            if key_code in (wx.WXK_END, wx.WXK_NUMPAD_END):
+            if key_code in (wal.KEY_END, wal.KEY_NUMPAD_END):
                 self.canvas.controller.key_ctrl_end(True)
                 return
-        elif modifiers == wx.ACCEL_SHIFT:
-            if key_code in (wx.WXK_LEFT, wx.WXK_NUMPAD_LEFT):
+        elif modifiers == wal.ACCEL_SHIFT:
+            if key_code in (wal.KEY_LEFT, wal.KEY_NUMPAD_LEFT):
                 self.canvas.controller.key_left(True)
                 return
-            if key_code in (wx.WXK_RIGHT, wx.WXK_NUMPAD_RIGHT):
+            if key_code in (wal.KEY_RIGHT, wal.KEY_NUMPAD_RIGHT):
                 self.canvas.controller.key_right(True)
                 return
-            if key_code in (wx.WXK_HOME, wx.WXK_NUMPAD_HOME):
+            if key_code in (wal.KEY_HOME, wal.KEY_NUMPAD_HOME):
                 self.canvas.controller.key_home(True)
                 return
-            if key_code in (wx.WXK_END, wx.WXK_NUMPAD_END):
+            if key_code in (wal.KEY_END, wal.KEY_NUMPAD_END):
                 self.canvas.controller.key_end(True)
                 return
-            if key_code in (wx.WXK_UP, wx.WXK_NUMPAD_UP):
+            if key_code in (wal.KEY_UP, wal.KEY_NUMPAD_UP):
                 self.canvas.controller.key_up(True)
                 return
-            if key_code in (wx.WXK_DOWN, wx.WXK_NUMPAD_DOWN):
+            if key_code in (wal.KEY_DOWN, wal.KEY_NUMPAD_DOWN):
                 self.canvas.controller.key_down(True)
                 return
 
@@ -147,7 +147,7 @@ class KbdProcessor:
 
     def on_char(self, event):
         modifiers = event.GetModifiers()
-        if modifiers not in (wx.ACCEL_CTRL, wx.ACCEL_CTRL | wx.ACCEL_SHIFT):
+        if modifiers not in (wal.ACCEL_CTRL, wal.ACCEL_CTRL | wal.ACCEL_SHIFT):
             if self.canvas.mode == modes.TEXT_EDIT_MODE:
                 self.canvas.controller.insert_text(unichr(event.GetUniChar()))
                 return
