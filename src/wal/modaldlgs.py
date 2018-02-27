fd85eda9f3c05d1e25a7cbd70ec8037324c42929
Igor Novikov <i.novikov@scalr.com>
2018-02-27 15:47:18 +0200

    progress dialog update fix

--- src/wal/modaldlgs.py
+++ src/wal/modaldlgs.py
@@ -233,10 +233,12 @@ class CustomProgressDialog(SimpleDialog):
         return self.result
 
     def update_data(self, value, msg):
-        wx.Yield()
         self.label.set_text(msg)
+        self.label.Update()
         self.progressbar.set_value(value)
+        self.progressbar.Update()
         self.Update()
+        wx.Yield()
 
     def on_load(self, *args):
         self._timer.Stop()
