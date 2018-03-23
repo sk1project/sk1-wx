f26a766a1aa9d3bd9a7ad93e97be75c29a2d5720
Igor E. Novikov <sk1.project.org@gmail.com>
2018-03-23 07:33:53 +0200

    refactoring

--- src/uc2/__init__.py
+++ src/uc2/__init__.py
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import translator
+from uc2.utils import translator
 
 config = None
 appdata = None
