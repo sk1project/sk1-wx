588097ba2e8940deefe1672ad02c55bbeb4eab35
Maxim <maxim.s.barabash@gmail.com>
2018-04-14 22:27:36 +0300

    update document info dialog

--- src/sk1/dialogs/docinfodlg.py
+++ src/sk1/dialogs/docinfodlg.py
@@ -60,6 +60,7 @@ class DocInfoDialog(wal.CloseDialog):
                 info['path_closed'] += 1
                 nodes = len(path[1])
             else:
+                info['path_opened'] += 1
                 nodes = len(path[1]) + 1
             info['max_path_point'] = max(nodes, info['max_path_point'])
             info['node'] += nodes
@@ -76,7 +77,7 @@ class DocInfoDialog(wal.CloseDialog):
         doc_name = self.doc.doc_name
         doc_file = self.doc.doc_file
         data = [
-            [_('File')],
+            _('File'),
             [_('Name and location:'), doc_file or doc_name]
         ]
         if doc_file and os.path.lexists(doc_file):
@@ -90,18 +91,16 @@ class DocInfoDialog(wal.CloseDialog):
         return data
 
     def document_info(self, objects):
-        pages = self.doc.get_pages()
-        layers = list(self.get_layers())
         units = self.doc.model.doc_units
         page_format = self.doc.methods.get_default_page_format()
         width = pt_to_units(page_format[1][0], units)
         height = pt_to_units(page_format[1][1], units)
-        page_size = "%s (%s x %s %s)" % (page_format[0], width, height, _(units))
+        page_size = (page_format[0], width, height, _(units))
         data = [
-            [_('Document')],
-            [_('Pages:'), len(pages)],
-            [_('Layers:'), len(layers)],
-            [_('Page size:'), page_size],
+            _('Document'),
+            [_('Pages:'), len(self.doc.get_pages())],
+            [_('Layers:'), len(list(self.get_layers()))],
+            [_('Page size:'), "%s (%s x %s %s)" % page_size],
             [_('Page orientation:'), uc2const.ORIENTS_NAMES[page_format[2]]]
         ]
         return data
@@ -110,46 +109,39 @@ class DocInfoDialog(wal.CloseDialog):
         if info is None:
             info = defaultdict(int)
         for obj in items:
-            if obj.is_text() or obj.is_pixmap():
+            if obj.is_text or obj.is_pixmap:
                 continue
-            info['object'] += 1
-            if obj.is_curve():
+            if obj.is_group:
+                self._walk_objects(obj.childs, info)
+            elif obj.is_curve:
                 self._analyze_path(obj, info)
-            elif obj.is_rect():
-                info['rect'] += 1
-            elif obj.is_circle():
-                info['circle'] += 1
-            elif obj.is_polygon():
-                info['polygon'] += 1
-            elif obj.is_primitive():
-                info['primitive'] += 1
-            elif obj.is_container():
-                info['container'] += 1
-                self._walk_objects(obj.childs, info)
-            elif obj.is_group():
-                info['group'] += 1
-                self._walk_objects(obj.childs, info)
+            props = ['is_curve', 'is_rect', 'is_circle', 'is_polygon',
+                     'is_group', 'is_container', 'is_primitive']
+            for name in props:
+                if getattr(obj, name):
+                    info[name] += 1
         return info
 
     def objects_info(self, objects):
         info = self._walk_objects(objects)
-        subpaths = info['subpaths']
-        closed = info['path_closed']
-        opened = info['subpaths'] - info['path_closed']
         units = self.doc.model.doc_units
-        path_length = pt_to_units(info['path_length'], units)
-        curve_subpaths = _('%i, opened %i, closed %i, length %g %s') % (subpaths, opened, closed, path_length, units)
+        subpaths_tmpl = _('%i, opened %i, closed %i, length %g %s')
+        subpaths = (
+            info['subpaths'], info['path_opened'], info['path_closed'],
+            pt_to_units(info['path_length'], units),
+            units
+        )
         data = [
-            [_('Graphic Objects')],
-            [_('Number of objects:'), info['object']],
+            _('Graphic Objects'),
+            [_('Number of objects:'), info['is_primitive']],
             [_('Number of points:'), info['node']],
             [_('Max. # of curve points:'), info['max_path_point']],
-            [_('Max. # of curve subpaths:'), curve_subpaths],
-            [_('Groups:'), info['group']],
-            [_('Rectangles:'), info['rect']],
-            [_('Ellipses:'), info['circle']],
-            [_('Containers:'), info['container']],
-            [_('Polygons:'), info['polygon']]
+            [_('Max. # of curve subpaths:'), subpaths_tmpl % subpaths],
+            [_('Groups:'), info['is_group'] - info['is_container']],
+            [_('Rectangles:'), info['is_rect']],
+            [_('Ellipses:'), info['is_circle']],
+            [_('Containers:'), info['is_container']],
+            [_('Polygons:'), info['is_polygon']]
         ]
         return data
 
@@ -157,9 +149,9 @@ class DocInfoDialog(wal.CloseDialog):
         if info is None:
             info = defaultdict(int)
         for obj in items:
-            if obj.is_group():
+            if obj.is_group:
                 self._walk_text(obj.childs, info)
-            elif obj.is_text() and obj.is_textblock():
+            elif obj.is_text and obj.is_textblock:
                 info['textblock'] += 1
                 self._analyze_text(obj, info)
                 fonts = info.setdefault('font', set())
@@ -171,7 +163,7 @@ class DocInfoDialog(wal.CloseDialog):
 
     def text_info(self, objects):
         info = self._walk_text(objects)
-        data = [[_('Text Statistics')]]
+        data = [_('Text Statistics')]
         if not info['textblock']:
             data.append([_('No Text in this document.')])
         else:
@@ -191,9 +183,9 @@ class DocInfoDialog(wal.CloseDialog):
         if info is None:
             info = defaultdict(list)
         for obj in items:
-            if obj.is_group():
+            if obj.is_group:
                 self._walk_bitmap(obj.childs, info)
-            elif obj.is_pixmap():
+            elif obj.is_pixmap:
                 pixmap = info.setdefault('pixmap', [])
                 pixmap.append(obj)
         return info
@@ -201,15 +193,15 @@ class DocInfoDialog(wal.CloseDialog):
     def bitmap_info(self, objects):
         info = self._walk_bitmap(objects)
         data = [
-            [_('Bitmap Objects')],
+            _('Bitmap Objects'),
         ]
         items = info.get('pixmap', [])
         if not items:
             data.append([_('No Bitmaps in this document.')])
         for i in items:
             dpi_w, dpi_h = i.get_resolution()
-            val = _('%s (%s x %s dpi), %s bytes') % (i.colorspace, dpi_w, dpi_h, len(i.bitmap))
-            data.append(['', val])
+            val = (i.colorspace, dpi_w, dpi_h, len(i.bitmap))
+            data.append(['', _('%s (%s x %s dpi), %s bytes') % val])
         return data
 
     def _walk_fill(self, items, info=None):
@@ -217,9 +209,9 @@ class DocInfoDialog(wal.CloseDialog):
             info = defaultdict(int)
         color_space = info.setdefault('color_space', defaultdict(int))
         for obj in items:
-            if obj.is_pixmap():
+            if obj.is_pixmap:
                 continue
-            if obj.is_group():
+            if obj.is_group:
                 self._walk_fill(obj.childs, info)
             else:
                 fill_style = obj.style[0]
@@ -242,14 +234,15 @@ class DocInfoDialog(wal.CloseDialog):
     def fill_info(self, objects):
         info = self._walk_fill(objects)
         data = [
-            [_('Fills')],
+            _('Fills'),
             [_('No fill:'), info['no_fill']],
             [_('Solid:'), info['fill_solid']],
             [_('Gradients:'), info['fill_gradient']],
             [_('Patterns:'), info['fill_pattern']],
-            [_('Objects and Color models')]
         ]
         color_space = info['color_space']
+        if color_space:
+            data.append([_('Objects and Color models')])
         for space_name in color_space:
             data.append(['\t%s' % space_name, color_space[space_name]])
         return data
@@ -259,9 +252,9 @@ class DocInfoDialog(wal.CloseDialog):
             info = defaultdict(int)
         color_space = info.setdefault('color_space', defaultdict(int))
         for obj in items:
-            if obj.is_pixmap():
+            if obj.is_pixmap:
                 continue
-            if obj.is_group():
+            if obj.is_group:
                 self._walk_stroke(obj.childs, info)
             else:
                 stroke_style = obj.style[1]
@@ -278,29 +271,26 @@ class DocInfoDialog(wal.CloseDialog):
     def stroke_info(self, objects):
         info = self._walk_stroke(objects)
         data = [
-            [_('Strokes')],
+            _('Strokes'),
             [_('No stroke:'), info['no_stroke']],
             [_('Solid:'), info['stroke_solid']],
-            [_('Objects and Color models')]
         ]
         color_space = info['color_space']
+        if color_space:
+            data.append([_('Objects and Color models')])
         for space_name in color_space:
             data.append(['\t%s' % space_name, color_space[space_name]])
         return data
 
     def build(self):
-        block = []
         data = [[_('Property'), _('Value')]]
-        r = ['file_info', 'document_info', 'objects_info', 'text_info',
-             'bitmap_info', 'fill_info', 'stroke_info']
-        for item in r:
-            block.append(len(data) - 1)
+        sections = [
+            'file_info', 'document_info', 'objects_info', 'text_info',
+            'bitmap_info', 'fill_info', 'stroke_info'
+        ]
+        for item in sections:
             data += getattr(self, item)(self.get_objects())
-        slist = wal.ReportList(self, data, alt_color=False)
-        for idx in block:
-            list_item = slist.GetItem(idx)
-            list_item.SetBackgroundColour(slist.even_color)
-            slist.SetItem(list_item)
+        slist = wal.ReportList(self, data)
         self.pack(slist, expand=True, fill=True, padding_all=5)
 
 
