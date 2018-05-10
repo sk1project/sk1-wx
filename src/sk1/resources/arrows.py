56619e696f050b854e9655c6c3df4da832c9ddba
Igor E. Novikov <sk1.project.org@gmail.com>
2018-05-10 11:01:19 +0300

    arrow data module

---
+++ src/sk1/resources/arrows.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2018 by Igor E. Novikov
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ARROWS = [
+[[[-0.27033587598424824, -0.500848400807721],
+  [[-2.8254854822834607, -2.1585491882092955],
+   [-2.551091781496059, -2.581194857500634],
+   [-2.2769815452755866, -3.004123991358902],
+   [1.7005932578740193, -0.4237460386029967],
+   [2.3525617618110273, -0.0005334401777999376],
+   [1.7005932578740193, 0.42211222911353863],
+   [-2.2769815452755866, 3.00050592990094],
+   [-2.8252020177165313, 2.1546476621844044],
+   [-0.27146973425196474, 0.499214591318263],
+   [-0.27033587598424824, -0.500848400807721]], 1]],
+
+]
