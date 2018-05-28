f870f012a593be3a0d1b919016b9253af7d256cb
Igor E. Novikov <sk1.project.org@gmail.com>
2018-05-29 00:25:22 +0300

    copyrights fixes

--- dependencies.py
+++ dependencies.py
@@ -2,7 +2,7 @@
 #
 #   Setup dependencies module
 #
-# 	Copyright (C) 2016 by Igor E. Novikov
+# 	Copyright (C) 2016-2018 by Igor E. Novikov
 #
 # 	This program is free software: you can redistribute it and/or modify
 # 	it under the terms of the GNU General Public License as published by
--- setup-sk1.py
+++ setup-sk1.py
@@ -2,7 +2,7 @@
 #
 #   Setup script for sK1 2.x
 #
-# 	Copyright (C) 2013-2016 by Igor E. Novikov
+# 	Copyright (C) 2013-2018 by Igor E. Novikov
 #
 # 	This program is free software: you can redistribute it and/or modify
 # 	it under the terms of the GNU General Public License as published by
