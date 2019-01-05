c1e3c0a6ebf121472c392682caff08187d38d16e
Igor E. Novikov <sk1.project.org@gmail.com>
2019-01-05 03:36:35 +0200

    msi build fix

--- bbox.py
+++ bbox.py
@@ -139,8 +139,8 @@ IMAGES = [
 ]
 
 LOCAL_IMAGES = [
-    # 'ubuntu_16.04_64bit',
-    'msw-packager',
+    'ubuntu_16.04_64bit',
+    # 'msw-packager',
 ]
 
 def clear_folders():
@@ -397,7 +397,7 @@ MSI_DATA = {
 if PROJECT == SK1:
     MSI_DATA['_Shortcuts'] = [
         {'Name': 'sK1 %s illustration program' % APP_VER,
-         'Description': 'Open source vector graphics editor',
+         'Description': 'Open source sK1 vector graphics editor',
          'Target': 'sk1.exe',
          'AddOnDesktop': True,
          'Open': [],
