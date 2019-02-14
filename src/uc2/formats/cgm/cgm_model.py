50f1cc3bfb6194a09d8d853c9a6e940d0991829c
Igor E. Novikov <sk1.project.org@gmail.com>
2019-02-14 23:08:48 +0200

    refactored markup

--- src/uc2/formats/cgm/cgm_model.py
+++ src/uc2/formats/cgm/cgm_model.py
@@ -15,7 +15,6 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from uc2 import utils
 from uc2.formats.cgm import cgm_const, cgm_utils
 from uc2.formats.generic import BinaryModelObject
 
@@ -51,72 +50,17 @@ class CgmElement(BinaryModelObject):
             self.element_id, hex(self.element_id)), 0
 
     def update_for_sword(self):
-        cgm_cls_name = cgm_const.CGM_CLS.get(self.element_class, '')
-        msg = 'Command Header\n' \
-              '  %d - element class\n' \
-              '  (%s)\n' \
-              '  0x%04x - element id\n' \
-              '  %d - parameter list size (bytes)' \
-              % (self.element_class, cgm_cls_name,
-                 self.element_id, self.params_sz)
-        msg += '\n' + '.' * 35
-        self.cache_fields = [(0, len(self.command_header), msg), ]
-
-    def do_update(self, presenter=None, action=False):
-        BinaryModelObject.do_update(self, presenter, action)
-        if action and self.is_padding:
-            self.cache_fields.append((len(self.chunk) - 1, 1, 'padding byte'))
+        self.cache_fields = cgm_utils.get_markup(
+            self.command_header, self.params)
 
 
-class CgmBeginMetafile(CgmElement):
-    title_sz = 0
-    title = ''
-
-    def __init__(self, command_header, params):
-        CgmElement.__init__(self, command_header, params)
-        if params:
-            self.title_sz = utils.byte2py_int(self.params[0])
-            self.title = self.params[1:]
-
-    def update_for_sword(self):
-        CgmElement.update_for_sword(self)
-        pos = len(self.command_header)
-        if self.params:
-            self.cache_fields += [(pos, 1, 'text length'),
-                                  (pos + 1, self.title_sz, 'file title')]
-
-
-class CgmMetafileVersion(CgmElement):
-    def __init__(self, command_header, params):
-        CgmElement.__init__(self, command_header, params)
-
-    def update_for_sword(self):
-        CgmElement.update_for_sword(self)
-        pos = len(self.command_header)
-        if self.params:
-            self.cache_fields += [(pos, len(self.params), 'version'), ]
-
-
-class CgmMetafileDescription(CgmElement):
-    def __init__(self, command_header, params):
-        CgmElement.__init__(self, command_header, params)
-        if params:
-            pass
-
-    def update_for_sword(self):
-        CgmElement.update_for_sword(self)
-        pos = len(self.command_header)
-        if self.params:
-            self.cache_fields += [(pos, len(self.params), 'version'), ]
-
+USE_FACTORY = False
 
 ID_TO_CLS = {
-    0x0020: CgmBeginMetafile,
-    0x1020: CgmMetafileVersion,
-    0x1040: CgmMetafileDescription,
 }
 
 
 def element_factory(header, params):
     element_id = parse_header(header)[1]
-    return ID_TO_CLS.get(element_id, CgmElement)(header, params)
+    return ID_TO_CLS.get(element_id, CgmElement)(header, params) \
+        if USE_FACTORY else CgmElement(header, params)
--- src/uc2/formats/cgm/cgm_utils.py
+++ src/uc2/formats/cgm/cgm_utils.py
@@ -16,6 +16,7 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from uc2 import utils
+from uc2.formats.cgm import cgm_const
 
 
 def parse_header(chunk):
@@ -26,3 +27,33 @@ def parse_header(chunk):
     if len(chunk) == 4:
         size = utils.uint16(chunk[2:]) & 0x7fff
     return element_class, element_id, size
+
+
+def get_markup(header, params):
+    chunk = header + params
+    element_class, element_id, params_sz = parse_header(header)
+    is_padding = params_sz < len(params)
+    cgm_cls_name = cgm_const.CGM_CLS.get(element_class, '')
+    msg = 'Command Header\n' \
+          '  %d - element class\n' \
+          '  (%s)\n' \
+          '  0x%04x - element id\n' \
+          '  %d - parameter list size (bytes)' \
+          % (element_class, cgm_cls_name,
+             element_id, params_sz)
+    msg += '\n' + '.' * 35
+    markup = [(0, len(header), msg), ]
+    hdsz = len(header)
+
+    if element_id == cgm_const.BEGIN_METAFILE and params:
+        title_sz = utils.byte2py_int(params[0])
+        markup += [(hdsz, 1, 'text length'), (hdsz + 1, title_sz, 'file title')]
+    elif element_id == cgm_const.METAFILE_VERSION and params:
+        markup += [(hdsz, 2, 'version'), ]
+    elif element_id == cgm_const.METAFILE_DESCRIPTION and params:
+        markup += [(hdsz, 1, 'text length'),
+                   (hdsz + 1, params_sz, 'description'), ]
+
+    if is_padding:
+        markup += [(len(chunk) - 1, 1, 'padding byte')]
+    return markup
