1b07243da58f6d90e27e9c73f1fda79ba90f6664
Igor E. Novikov <sk1.project.org@gmail.com>
2019-02-15 10:20:02 +0200

    initial translator code

--- src/uc2/formats/cgm/cgm_translators.py
+++ src/uc2/formats/cgm/cgm_translators.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-#  Copyright (C) 2017 by Igor E. Novikov
+#  Copyright (C) 2017-2019 by Igor E. Novikov
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -15,9 +15,53 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from uc2.formats.cgm import cgm_const
+
 
 class CGM_to_SK2_Translator(object):
-    def translate(self, cgm_doc, sk2_doc): pass
+    sk2_doc = None
+    sk2_mtds = None
+    page = None
+    layer = None
+
+    def translate(self, cgm_doc, sk2_doc):
+        self.sk2_doc = sk2_doc
+        self.sk2_mtds = sk2_doc.methods
+        self.page = self.sk2_mtds.get_page()
+        self.layer = self.sk2_mtds.get_layer(self.page)
+
+        for element in cgm_doc.childs:
+            if element.element_id == cgm_const.END_METAFILE:
+                break
+            sig = cgm_const.CGM_ID.get(
+                element.element_id, '').replace(' ', '_').lower()
+            if sig:
+                mtd = getattr(self, '_' + sig, None)
+                if mtd:
+                    mtd(element)
+
+        self.sk2_doc = None
+        self.sk2_mtds = None
+        self.page = None
+        self.layer = None
+
+    def _rectangle(self, element):
+        pass
+
+    def _circle(self, element):
+        pass
+
+    def _ellipse(self, element):
+        pass
+
+    def _line(self, element):
+        pass
+
+    def _polyline(self, element):
+        pass
+
+    def _polygon(self, element):
+        pass
 
 
 class SK2_to_CGM_Translator(object):
