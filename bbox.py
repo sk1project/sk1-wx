720fc9beb7b303e250bae6bfc1163889f8e03268
Igor E. Novikov <i.novikov@scalr.com>
2019-06-17 03:08:06 +0300

    build refactoring wip

--- bbox.py
+++ bbox.py
@@ -192,10 +192,6 @@ def set_build_stamp():
 
 
 def pull_images():
-    echo_msg(os.environ['RHOST'][:2])
-    echo_msg(os.environ['RHOST'][2:])
-    echo_msg(os.environ['SSHPASS'][:2])
-    echo_msg(os.environ['SSHPASS'][2:])
     for image in IMAGES:
         msg = 'Pulling %s%s image' % (IMAGE_PREFIX, image)
         msg += ' ' * (50 - len(msg)) + '...'
