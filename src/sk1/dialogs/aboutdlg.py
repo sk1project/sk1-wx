759ef5e3f5995fa5d42acff36011ce3e32a3e425
Igor E. Novikov <i.novikov@scalr.com>
2020-01-25 13:47:46 +0200

    Italian translator creds

--- src/sk1/dialogs/aboutdlg.py
+++ src/sk1/dialogs/aboutdlg.py
@@ -115,6 +115,7 @@ TR_LIST = [
     ['LANG', 'Translators'],
     ['Brazilian Portuguese','João Lima Neto <contato@joaolimaneto.com.br>'],
     ['German', 'Thore Sommer <mail@thson.de>'],
+    ['Italian', 'Tom Spaccavento <spcvntom@gmail.com>'],
     ['Russian', 'Igor Novikov <sk1.project.org@gmail.com>'],
     ['Spanish', 'Carlos Jesús Atagua Díaz <atacarlos@gmail.com>'],
     ['Ukrainian', 'Maxim Barabash <maxim.s.barabash@gmail.com>,'],
